5de5eeef4dd396511fdf5378fe5f5d3a0804a752
Thomas Woinke <thomas@steckschwein.de>
2014-04-27 19:38:19 +0200

    deutsche tastaturbelegung, interface 8bit parallel an VIA

--- keyboard/avr-keyboard-interface/software/main.c
+++ keyboard/avr-keyboard-interface/software/main.c
@@ -8,12 +8,38 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <avr/io.h>
+#include <avr/delay.h>
 
 #include "keyboard.h"
 #include "keycodes.h"
 
+void pump(uint8_t b)
+{
+	uint8_t bit=7;
+
+	do
+	{
+		if (b & _BV(bit))
+		{
+			PORTA |= _BV(1);
+		}
+		else
+		{
+			PORTA &= ~_BV(1);
+		}
+		
+	;	_delay_ms(5);
+		PORTA |= _BV(0);
+	;	_delay_ms(5);
+		PORTA &= ~_BV(0);
+	
+	}
+	while(bit-- > 0);
+}	
+
 int main( void )
 {
+	
 	unsigned char key, tmp = 0;
 	
 	keyboardInit( );
@@ -21,53 +47,21 @@ int main( void )
 	DDRB = 0xff;
 	PORTB = 0x00;
 	
+	DDRA = 3;
+	PORTA=0;
+
 	sei( );
 
 	while( 1 )
 	{
-
 		if (( key = getKey( )) != 0 )
 		{
 			PORTB=key;
-			continue;
-
-			switch ( key )
-			{
-				case '1':
-				{
-					PORTB = 1;
-					break;
-				}
-				case '2':
-				{
-					PORTB = 2;
-					break;
-				}
-				case '3':
-				{
-					PORTB = 3;
-					break;
-				}
-				case UP:
-				{
-					tmp = PINB & 0x03;
-					if ( ++tmp == 4 )
-					PORTB = tmp;
-				}
-				case DOWN:
-				{
-					tmp = PINB & 0x03;
-					if ( --tmp == 255 )
-						tmp = 0;
-					PORTB = tmp;
-				}
-				default:
-				{
-					PORTB = 0;
-				}
-			}
+			//pump(key);
+			_delay_ms(10);
 		}
 		PORTB=0;
+		//pump(0);
 	}
 	return 0;
 }
