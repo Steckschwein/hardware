c78ebb974c5d9285da61ab1ab3cc4757bbd85673
Marko Lauke <marko.lauke@googlemail.com>
2014-09-09 05:47:39 +0200

    +vc20 raw

--- charsets/charrom.sh
+++ charsets/charrom.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-#c=0 && echo -n '!byte ' > char.tmp.h.a && for l in `od -t x1 CHAR.ROM | grep -v "0010000" | cut -d ' ' -f2-`; do echo -n "\$$l"; c=`expr $c + 1`; if [ $c -eq 8 ]; then c=0 && echo; echo -n '!byte '; else echo -n ","; fi  ;done >> char.tmp.h.a
+c=0 && echo -n '!byte ' > char.tmp.h.a && for l in `od -t x1 characters.901460-03.bin | grep -v "0010000" | cut -d ' ' -f2-`; do echo -n "\$$l"; c=`expr $c + 1`; if [ $c -eq 8 ]; then c=0 && echo; echo -n '!byte '; else echo -n ","; fi  ;done >> char.tmp.h.a
 echo 'charset:' > char.ascii.h.a
 sed -n 65,96p char.rom.a >> char.ascii.h.a 	# 32 chars 
 sed -n 33,64p char.rom.a >> char.ascii.h.a	# 
