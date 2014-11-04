c24ea900b683d9cae90901a19032daa10d602b3f
Thomas Woinke <thomas@steckschwein.de>
2014-11-04 09:14:43 +0100

    eigene header files spi

---
+++ keyboard/avr/spi.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <avr/io.h>
+#include "spi.h"
+
+/*
+    Initialize USI as slave
+*/
+void spiInit()
+{
+	//DO pin is configured for output
+	CTRL_PORT |= _BV(DO_PIN);
+
+	// SS as input
+	CTRL_PORT &= ~(1 << SS_PIN);
+
+	// pullup on (DI)
+	DATA_PORT |= _BV(DI_PIN); 
+	
+	//Set three wire mode and set
+	//clock to External, positive edge.
+	// USICR = _BV(USIWM0) | (0 << USICS0) | _BV(USICS1);
+	USICR = _BV(USIWM0) | _BV(USICS1);
+	
+	//Clear overflow flag
+	USISR = _BV(USIOIF);
+}
+
+
+unsigned char spiTransfer(unsigned char val)
+{
+	USIDR = val;	
+	//Clear the overflow flag
+	USISR = _BV(USIOIF);
+
+	while ((USISR & (1 << USIOIF)) == 0) {}; // Do nothing until USI has data ready
+	return USIDR;
+}
+
+uint8_t spiSelected()
+{
+	return PINB & (0 << SS_PIN);
+}
---
+++ keyboard/avr/spi.h
@@ -0,0 +1,15 @@
+#ifndef SPI_H
+#define SPI_H
+
+#define CTRL_PORT   DDRB
+#define DATA_PORT   PORTB
+#define SS_PIN      PB4
+#define CLK_PIN     PB7
+#define DI_PIN      PB5
+#define DO_PIN      PB6
+
+void spiInit();
+unsigned char spiTransfer(unsigned char);
+uint8_t spiSelected();
+
+#endif
