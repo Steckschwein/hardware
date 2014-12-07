72b1d3e3b23ffa537e08d795dd0df785fd931225
Thomas Woinke <thomas@steckschwein.de>
2014-12-07 22:50:03 +0100

    cleanup

--- keyboard/avr313/spi.h
+++ keyboard/avr313/spi.h
@@ -1,15 +1,9 @@
 #ifndef SPI_H
 #define SPI_H
 
-// #define CTRL_PORT   DDRB
-// #define DATA_PORT   PORTB
-// #define SS_PIN      PB4
-// #define CLK_PIN     PB7
-// #define DI_PIN      PB5
-// #define DO_PIN      PB6
-
 #define DDR_SPI DDRB
 #define DD_MISO PB4
+
 void spiInitSlave();
 unsigned char spiTransfer(unsigned char);
 unsigned char spiEnabled();
