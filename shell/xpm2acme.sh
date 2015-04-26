074e82e1f9e073e8c4e6a6cc963386277a989645
Marko Lauke <marko.lauke@googlemail.com>
2015-04-26 09:54:12 +0200

    +update eieruhr

--- shell/xpm2acme.sh
+++ shell/xpm2acme.sh
@@ -6,4 +6,4 @@ if [ ! -e "$1" ]; then
 	echo "nicht so!, datei '$1' not found!"
 	exit -1;
 fi
-tail -n+6 $1 | cut -b 1-17 | tr '! ' '#.' | sed "s/\"/+SpriteLine %/g"
+tail -n+6 $1 | cut -b 1-17 | tr '! ' '#.' | sed "s/\"/+SpriteLine16 %/g"
