c9e8cab0653da2372d6b1dd39ea687ef9367e800
Thomas Woinke <thomas@steckschwein.de>
2015-05-11 08:14:36 +0200

    first working version, start address hardcoded to 000

--- transfer/transfer.c
+++ transfer/transfer.c
@@ -7,6 +7,7 @@
 #include <termios.h>  // POSIX terminal control definitions 
 
 #define BUFSIZE 65535
+#define BAUDRATE	B115200
 char * device = "/dev/cu.usbserial-FTAJMAUJ";
 char buffer[BUFSIZE];
 
@@ -28,90 +29,100 @@ int open_port(void)
           return (port);
 }
 
-
 int main(int argc, char *argv[])
 {
-          int port,n;
-          char str[30];
-          int length;
-          char * p = buffer;
+		int port,n;
+		
+		uint16_t length;
+		char buf[2];
 
-          FILE *fp; // input file
-          fp = fopen(argv[1], "r");
-          length = fread(buffer, 1, sizeof(buffer), fp);
-          fclose(fp);
+		FILE *fp; // input file
+		fp = fopen(argv[1], "r");
+		if (!fp )
+		{
+		printf("Open error %d: %s\n", errno, argv[1]);
+		return 1;
+		}
 
-          
-          //termios - structure contains options for port manipulation
-          struct termios specs; // for setting baud rate 
-
-          //setup part
-          port = open_port();
-          tcgetattr(port, &specs); 
-
-          //now the specs points to the opened port's specifications
-          specs.c_cflag = (CLOCAL | CREAD ); //control flags
+		length = fread(buffer, 1, sizeof(buffer), fp);
+		fclose(fp);
 
 
-          //output flags
-          //CR3 - delay of 150ms after transmitting every line
-          specs.c_oflag = (OPOST | CR3);
+		//termios - structure contains options for port manipulation
+		struct termios specs; // for setting baud rate 
+
+		//setup part
+		port = open_port();
+		tcgetattr(port, &specs); 
+
+		//now the specs points to the opened port's specifications
+		specs.c_cflag = (CLOCAL | CREAD ); //control flags
 
 
-         //set Baud Rate to 115200bps
-         cfsetospeed(&specs,B115200);
-         cfsetispeed(&specs,B115200);
+		//output flags
+		//CR3 - delay of 150ms after transmitting every line
+		specs.c_oflag = (OPOST | CR3);
 
 
-         //our custom specifications set to the port
-         //TCSANOW - constant that prompts the system to set
-         //specifications immediately.
-          tcsetattr(port,TCSANOW,&specs);
-         
-          //execution part
-         // printf("\nEnter the data:\t");
-         // scanf("%s",str);
-        str[0] = 0x00;
-        str[1] = 0x10;
-         n = write(port,str,2); // n = no of bytes written
-         if (n<0) {
-              printf("\nError");
-         }
-
-        read(port, str, 2);
-        if (strncmp(str, "OK", 2) != 0)
-        {
-            printf("ERROR1\n");
-            exit(1);
-        }
-
-        str[0] = length;
-        str[1] = 0x00;
-        n = write(port,str,2); // n = no of bytes written
-        if (n<0) {
-            printf("\nError");
-        }
-
-        read(port, str, 2);
-        if (strncmp(str, "OK", 2) != 0)
-        {
-            printf("ERROR2\n");
-            exit(1);
-        }
+		//set Baud Rate to 115200bps
+		cfsetospeed(&specs,BAUDRATE);
+		cfsetispeed(&specs,BAUDRATE);
 
 
-        n = write(port, buffer, length);
-        read(port, str, 2);
-        if (strncmp(str, "OK", 2) != 0)
-        {
-            printf("ERROR2\n");
-            exit(1);
-        }
+		//our custom specifications set to the port
+		//TCSANOW - constant that prompts the system to set
+		//specifications immediately.
+		tcsetattr(port,TCSANOW,&specs);
 
-        printf("%d bytes written.\n", n);
+	
+		// Send start address 0x1000
+		uint16_t startaddr = 0x1000;
+		
+		buf[0] = (uint8_t)startaddr;
+		buf[1] = (uint8_t)(startaddr >> 8);
+		n = write(port,buf,2); // n = no of bytes written
+		if (n<0) 
+		{
+		 	printf("\nError");
+		}
 
-         //close the port
-        close(port);
-        return(0);
+
+		read(port, buf, 2);
+		if (strncmp(buf, "OK", 2) != 0)
+		{
+			printf("Error sending startaddr\n");
+			exit(1);
+		}
+
+
+		buf[0] = (uint8_t)length;
+		buf[1] = (uint8_t)(length >> 8);
+
+		n = write(port,buf,2); // n = no of bytes written
+		if (n<0) {
+			printf("\nError");
+		}
+
+		read(port, buf, 2);
+		if (strncmp(buf, "OK", 2) != 0)
+		{
+			printf("Error sending length\n");
+			exit(1);
+		}
+
+
+		n = write(port, buffer, length);
+		read(port, buf, 2);
+		if (strncmp(buf, "OK", 2) != 0)
+		{
+			printf("Error sending data at byte %d\n", n);
+			exit(1);
+		}
+
+		printf("%d bytes written.\n", n);
+
+		//close the port
+		close(port);
+		return(0);
 } 
 
