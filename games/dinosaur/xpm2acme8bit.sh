21222ec79448c4d0dbd52654999be5fc6e7d362d
Marko Lauke <marko.lauke@googlemail.com>
2015-08-07 22:00:40 +0200

    +dinosaur initial, to be continued...

---
+++ games/dinosaur/xpm2acme8bit.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# 
+# xpm output from iconmaker
+#
+if [ ! -e "$1" ]; then
+	echo "nicht so!, datei '$1' not found!"
+	exit -1;
+fi
+tail -n+6 $1 | cut -b 1-9 | tr '! ' '#.' | sed "s/\"/+SpriteLine %/g"
