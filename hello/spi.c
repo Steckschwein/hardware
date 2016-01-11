ad0ade19604cc192471fc3cdb2b337cb7777ef00
Thomas Woinke <thomas@steckschwein.de>
2016-01-11 18:58:01 +0100

    hex

--- hello/spi.c
+++ hello/spi.c
@@ -6,9 +6,9 @@ main ()
 	char c;
 	while(1)
 	{
-		*(unsigned char*) 0x210 = 0b01111010;
+		*(unsigned char*) 0x210 = 0x7A;
 		c = spi_read();
-		*(unsigned char*) 0x210 = 0b01111110;
+		*(unsigned char*) 0x210 = 0x7E;
 
 		cprintf("%x\n", c);
 	}
