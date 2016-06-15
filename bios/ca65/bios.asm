adacdda4419f754644dfa2786bdcc327f48c4ef4
Thomas Woinke <thomas@steckschwein.de>
2016-06-15 20:45:09 +0200

    separate nvram

--- bios/ca65/bios.asm
+++ bios/ca65/bios.asm
@@ -11,6 +11,7 @@
 .import init_vdp, vdp_chrout
 .import init_sdcard
 .import fat_mount, fat_read, fat_find_first, calc_lba_addr
+.import read_nvram
 
 .segment "BIOS"
 
@@ -234,51 +235,7 @@ startup:
 
 
 
-;---------------------------------------------------------------------
-; read 96 bytes from RTC as parameter buffer
-;---------------------------------------------------------------------
-read_nvram:
-	save
-	; select RTC
-	lda #%01110110
-	sta via1portb
 
-	lda #$20
-	jsr spi_rw_byte
-
-	ldx #$00
-@l1:	
-	phx
-	lda #$ff
-	jsr spi_rw_byte
-	plx
-	sta nvram,x
-	inx
-	cpx #96
-	bne @l1
-
-	; deselect all SPI devices
-	lda #%01111110
-	sta via1portb
-
-
-	lda #$42
-	cmp nvram + param_sig
-	bne @invalid_sig
-
-	SetVector nvram, paramvec
-
-@exit:
-	restore
-	rts
-
-@invalid_sig:
-	jsr print_crlf
-	printstring "NVRAM: Invalid signature."
-	bra @exit
-; .nvram_crc_error
-; 	+PrintString .txt_nvram_crc_error
-; 	bra -
 
 dummy_irq:
 		rti
---
+++ bios/ca65/nvram.asm
@@ -0,0 +1,50 @@
+.setcpu "65c02"
+.export read_nvram
+.import spi_rw_byte, print_crlf, primm
+.include "bios.inc"
+.segment "BIOS"
+
+;---------------------------------------------------------------------
+; read 96 bytes from RTC as parameter buffer
+;---------------------------------------------------------------------
+read_nvram:
+	save
+	; select RTC
+	lda #%01110110
+	sta via1portb
+
+	lda #$20
+	jsr spi_rw_byte
+
+	ldx #$00
+@l1:	
+	phx
+	lda #$ff
+	jsr spi_rw_byte
+	plx
+	sta nvram,x
+	inx
+	cpx #96
+	bne @l1
+
+	; deselect all SPI devices
+	lda #%01111110
+	sta via1portb
+
+
+	lda #$42
+	cmp nvram + param_sig
+	bne @invalid_sig
+
+	SetVector nvram, paramvec
+
+@exit:
+	restore
+	rts
+@invalid_sig:
+	jsr print_crlf
+	printstring "NVRAM: Invalid signature."
+	bra @exit
+; .nvram_crc_error
+; 	+PrintString .txt_nvram_crc_error
+; 	bra -
