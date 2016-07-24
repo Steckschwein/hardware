b059a3e76c913160f94fcb74e5ee3332c49cb987
Thomas Woinke <thomas@steckschwein.de>
2016-07-24 09:41:07 +0200

    moved to firmware

---
+++ keyboard/avr313/kb.h
@@ -0,0 +1,32 @@
+// Keyboard communication routines
+
+#ifndef __KB_INCLUDED
+#define __KB_INCLUDED
+
+// Keyboard konnections
+#define PIN_KB  PIND
+#define PORT_KB PORTD
+#define CLOCK   2
+#define DATAPIN 0
+
+void init_kb(void);
+void decode(unsigned char sc);
+void put_kbbuff(unsigned char c);
+// int  get_kbchar(void);
+void send_kb(uint8_t);
+void put_scanbuff(unsigned char c);
+int  get_scanchar(void);
+
+#define SCAN_BUFF_SIZE 32
+ uint8_t scan_buffer[SCAN_BUFF_SIZE];
+ uint8_t *scan_inptr;
+ uint8_t *scan_outptr;
+ uint8_t scan_buffcnt;
+
+#define KB_BUFF_SIZE 16
+ uint8_t kb_buffer[KB_BUFF_SIZE];
+ uint8_t *kb_inptr;
+ uint8_t *kb_outptr;
+ uint8_t kb_buffcnt;
+
+#endif
---
+++ keyboard/avr313/main.c
@@ -0,0 +1,67 @@
+#include <avr/io.h>
+#include <avr/interrupt.h>
+#include <util/delay.h>
+
+
+#include "spi.h"
+#include "kb.h"
+
+
+/* -------------------------------------------------------------------
+Design:
+1. 	INT0 für Tastaturabfrage hat IMMER prio
+	INT0-ISR stopft scancodes in scancode buffer
+
+2. 	Mainloop 
+	- dekodiert Scancodes aus scancode-Buffer 
+	- Lookup ASCII Zeichen (decode())
+	- Zeichen in Zeichenpuffer
+	
+3.  SPI-Transport in Interrupt
+-------------------------------------------------------------------*/
+
+
+
+/* -------------------------------------------------------------------
+	Atmel application note AVR313 ported for use with GCC
+	by Mike Henning - September 2008
+
+	Changes: 
+	- Modified to use the ATMEGA16 instead of the obsolete 90S8515.
+	- Changed the scan code table for english keyboards
+	- Baud rate can be set under project configuration in Avr Studio.
+	- The clr screen routine is not implemented - did not see the
+	  point here.
+---------------------------------------------------------------------
+	A few improvements could be made:
+
+	-Check kb parity bit and implement a timeout for keyboard scan
+	-Move the call to decode out of the ISR
+	-Add support for sending commands to the keyboard
+	-Add support for extended keys
+
+-------------------------------------------------------------------*/
+
+
+int __attribute__((OS_main noreturn)) main(void)
+{
+	uint8_t tmp;
+    
+    cli();
+	init_kb();
+	spiInitSlave();
+	sei();
+
+	while(1)
+	{		
+		tmp = get_scanchar();
+		if (tmp != 0)
+		{
+			decode(tmp);
+		}
+		
+	}
+	// return 0;
+}
+
+
