1a25d3c2c08445e7d6db08e6d3440eb7af485908
Thomas Woinke <thomas@steckschwein.de>
2016-10-12 21:47:30 +0200

    cleanup

--- steckos/kernel/shell.asm
+++ steckos/kernel/shell.asm
@@ -49,7 +49,8 @@ init:
 
 	printstring "SD card init error"
 
-	jmp hello
+	bra hello
+
 
 @l1:
 	jsr krn_mount
@@ -57,14 +58,19 @@ init:
 	beq @l2
 
 	printstring "SD card mount error"
-	jmp hello
+	bra hello
 
 @l2: 
 
 	SetVector mainloop, retvec
 	SetVector buf, bufptr
 
-	jmp	hello
+
+hello:
+	crlf
+	jsr krn_primm
+	.asciiz "SteckShell 0.12c"
+	crlf
 
 mainloop:
 	; output prompt character
@@ -257,17 +263,6 @@ printbuf:
 
 
 cmdlist:
-	; .byte "dir"
-	; .byte $00
-	; .word dir
-
-	; .byte "ll"
-	; .byte $00
-	; .word dir
-
-	; .byte "ls"
-	; .byte $00
-	; .word ls
 
 	.byte "cd"
 	.byte $00	
@@ -276,10 +271,6 @@ cmdlist:
 	.byte "up",0
 	.word krn_upload
     
-	; !text "init"
-	; !byte $00	
-	; !word .init
-
 	.byte "help"
 	.byte $00	
 	.word help
@@ -305,152 +296,6 @@ atoi:
 		sbc #$30
 		rts
 
-param2fileptr:
- 	copypointer paramptr, filenameptr
- 	rts
-
-
-; dir_show_entry_short:
-; 		pha
-; 		jsr print_filename
-; 		lda #' '
-; 		jsr krn_chrout
-; 		lda #' '
-; 		jsr krn_chrout
-
-; 		dec entries
-; 		bne @l1	
-; 		crlf
-; 		lda #num_ls_entries
-; 		sta entries
-; @l1:
-; 		pla
-; 		rts
-
-; dir_show_entry:
-; 		pha
-; 		jsr print_filename
-	
-; 		lda #' '
-; 		jsr krn_chrout
-
-; 		ldy #DIR_Attr
-; 		lda (dirptr),y
-
-
-; 		ror
-; 		ror
-; 		ror
-; 		bcc @l1
-; 		lda #'V'
-; 		bra @l4
-; @l1:
-; 		ror
-; 		bcc @l2
-; 		lda #'S'
-; 		bra @l4
-; @l2:
-; 		ror
-; 		bcc @l3
-; 		lda #'D'
-; 		bra @l4
-; @l3:
-; 		lda #'F'	
-; @l4:
-; 		jsr krn_chrout
-; 		lda #' '
-; 		jsr krn_chrout
-		
-; 		ldy #DIR_FileSize + 1 +1
-; @l5:	dey
-; 		lda (dirptr),y
-; 		jsr krn_hexout
-
-; 		cpy #DIR_FileSize
-; 		bne @l5
-
-; 		lda #' '
-; 		jsr krn_chrout
-
-; 		ldy #DIR_WrtDate 
-; 		lda (dirptr),y
-; 		and #%00011111
-; 		jsr decoutz
-	
-; 		; month
-; 		iny
-; 		lda (dirptr),y
-; 		lsr
-; 		tax
-; 		dey
-; 		lda (dirptr),y
-; 		ror
-; 		lsr
-; 		lsr
-; 		lsr
-; 		lsr
-		
-; 		jsr decoutz
-		
-; 		; +PrintChar '.'
-; 		; year
-; 		txa
-; 		clc 
-; 		adc #80   	; add begin of msdos epoch (1980)
-; 		cmp #100	
-; 		bcc @l6		; greater than 100 (post-2000)
-; 		sec 		; yes, substract 100
-; 		sbc #100
-; @l6:	jsr decoutz ; there we go
-
-	
-; 		lda #' '
-; 		jsr krn_chrout
-
-
-; 		ldy #DIR_WrtTime +1
-; 		lda (dirptr),y
-; 		tax
-; 		lsr
-; 		lsr
-; 		lsr
-	
-; 		jsr decoutz
-
-; 		lda #':'
-; 		jsr krn_chrout
-
-
-; 		txa
-; 		and #%00000111
-; 		sta tmp1
-; 		dey
-; 		lda (dirptr),y
-
-; 		.repeat 5	
-; 		lsr tmp1
-; 		ror 
-; 		.endrepeat
-
-; 		jsr decoutz
-
-
-;         ; Bits 11–15: Hours, valid value range 0–23 inclusive.
-;         crlf
-	
-; 		pla
-; 		rts	
-
-; print_filename:
-; 		ldy #DIR_Name
-; @l1:	lda (dirptr),y
-; 		jsr krn_chrout
-; 		iny
-; 		cpy #$0b
-; 		bne @l1
-; 		rts
-
-
 
 errmsg:
         ;TODO FIXME maybe use oserror() from cc65 lib
@@ -474,80 +319,12 @@ errmsg:
 		jmp mainloop
 
 
-; .hellotxt		!text "SteckShell 0.11 ",$00
 helptxt1:
 				.byte $0a,$0d,"ll/ls       - directory (long/short)"
 				.byte $0a,$0d,"cd <name>   - change directory"
 				; !text $0a,$0d,"run <name>  - run program"		
 				.byte $0a,$0d,"dump <addr> <addr> - dump memory"
 				.byte $00
-
-; .crlf			!byte $0a,$0d,$00
-; .prompt			!text $0a,$0d,">",$00
-
-; .txt_msg_param_error	!text $0a,$0d,"parameter error",$0a,$0d,$00
-; .txt_msg_sd_init_error	!text $0a,$0d,"sdcard init error",$0a,$0d,$00
-; .txt_cd 				!text "cd ok",$00
-
-; .fat_err_signature 		!text "bad block signature", $00
-; .fat_err_partition 		!text "invalid partition type", $00
-; .fat_err_bad_sect_size 	!text "sector size unsupported", $00
-; .fat_err_open			!text "open error",$00
-; .fat_err_no_such_file	!text "no such file or directory",$00
-; .fat_err_file_not_open	!text "file not open error",$00
-; .fat_err_too_many_open	!text "too many open files",$00
-dir_entry:
-		jmp (entryvec)
-
-; ls:
-; 		lda #num_ls_entries
-; 		sta entries
-; 		SetVector dir_show_entry_short, entryvec
-
-; 		bra l1
-; dir:
-; 		SetVector dir_show_entry, entryvec
-; l1:
-; 		crlf
-; 		SetVector pattern, filenameptr
-
-; 		lda (paramptr)
-; 		beq @l2
-; 		copypointer paramptr, filenameptr
-
-; @l2:
-;         ldx #0
-; 		jsr krn_find_first
-;         lda errno
-;         beq @l2_1
-;         printstring "i/o error"
-;         jmp mainloop
-        
-; @l2_1:	bcs @l4
-; 		bra @l5
-; 		; jsr .dir_show_entry
-; @l3:
-; 		jsr krn_find_next
-; 		bcc @l5
-; @l4:	
-; 		lda (dirptr)
-; 		cmp #$e5
-; 		beq @l3
-
-; 		ldy #DIR_Attr
-; 		lda (dirptr),y
-
-; 		bit #dir_attrib_mask ; Hidden attribute set, skip
-; 		bne @l3
-
-; 		jsr dir_entry
-
-; 		jsr krn_getkey
-; 		cmp #$03 ; CTRL-C?
-; 		beq @l5
-; 		bra @l3
-; @l5:
-; 		jmp mainloop
 
 
 cd:
@@ -618,18 +395,6 @@ run:
         bne @try_path
         lda #$fe
         jmp errmsg
-
-; 		ldx #$00
-; printloop:
-; 		lda tmpbuf,x
-; 		beq back
-; 		jsr krn_chrout
-; 		inx
-; 		bra printloop
-
-
-back:
-        jmp mainloop
 
 
 dumpvec 		= $c0
@@ -740,13 +505,6 @@ upload:
 	; jump to new code
 	jmp (startaddr)	
 
-hello:
-	crlf
-	jsr krn_primm
-	.asciiz "SteckShell 0.12b"
-	crlf
-	jmp mainloop
-
 help:
 	crlf
 	; +ShellPrintString .hellotxt
@@ -759,61 +517,5 @@ init_textui:
 	jsr	krn_textui_init
 	jsr	krn_textui_enable
 	rts
-; ;----------------------------------------------------------------------------------------------
-; ; decout - output byte in A as decimal ASCII without leading zeros
-; ;----------------------------------------------------------------------------------------------
-; decout:
-; 		phx
-; 		phy
-; 		ldx #1
-; 		stx tmp1
-; 		inx
-; 		ldy #$40
-; @l1:
-; 		sty tmp0
-; 		lsr
-; @l2:	rol
-; 		bcs @l3
-; 		cmp dec_tbl,x
-; 		bcc @l4
-; @l3:	sbc dec_tbl,x
-; 		sec
-; @l4:	rol tmp0
-; 		bcc @l2
-; 		tay
-; 		cpx tmp1
-; 		lda tmp0
-; 		bcc @l5
-; 		beq @l6
-; 		stx tmp1
-; @l5:	eor #$30
-; 		jsr krn_chrout
-; @l6:	tya
-; 		ldy #$10
-; 		dex
-; 		bpl @l1
-; 		ply
-; 		plx
-
-; 		rts
-; decoutz:
-; 		cmp #10
-; 		bcs @l1
-; 		pha
-; 		lda #'0'
-; 		jsr krn_chrout
-; 		pla
-; @l1:	
-; 		jmp decout
- 
-;  ; Lookup table for decimal to ASCII
-; dec_tbl:			.byte 128,160,200
-
-;pattern:			.byte "*.*",$00
 PATH:		        .asciiz "/bin/:/sbin/:/usr/bin/"
 tmpbuf:				
-
-;.segment "OS_CACHE"
-;.segment "KERNEL"
-;.segment "JUMPTABLE"
-;.segment "VECTORS"
