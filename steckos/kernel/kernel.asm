d660035e3d072cfa0956aac09a5d382610def21e
Thomas Woinke <thomas@steckschwein.de>
2016-11-24 22:43:20 +0100

    version bump

--- steckos/kernel/kernel.asm
+++ steckos/kernel/kernel.asm
@@ -41,7 +41,7 @@ kern_init:
     jsr primm
     .byte $d5,$cd,$cd,$cd,$cd,$cd,$cd,$cd,$cd,$cd,$cd,$cd,$cd
     .byte $cd,$cd,$cd,$cd,$cd,$cd,$cd,$cd,$b8,$0a,$0d
-    .byte $b3," SteckOS Kernel 0.5 ",$b3,$0a,$0d
+    .byte $b3," SteckOS Kernel 0.6 ",$b3,$0a,$0d
     .byte $d4,$cd,$cd,$cd,$cd,$cd,$cd,$cd,$cd,$cd,$cd,$cd,$cd
     .byte $cd,$cd,$cd,$cd,$cd,$cd,$cd,$cd,$be,$0a,$0d
     .byte $00
