828f4d8116af60f2b118789cbaefbc0cbb4ec2bb
Thomas Woinke <thomas@steckschwein.de>
2016-11-28 19:56:47 +0100

    removed unused code

--- ehbasic/steck_mon.asm
+++ ehbasic/steck_mon.asm
@@ -20,11 +20,7 @@ LF  = $0A        ; Line feed character
 ;IN    = $0200    ; Buffer used by GetLine. From $0200 through $027F (shared with Woz Mon)
 IN    =  $c000
 
-;SaveZeroPage    = $9140      ; Routines in CFFA1 firmware
-;RestoreZeroPage = $9135
-
 ; put the IRQ and MNI code in RAM so that it can be changed
-
 IRQ_vec	= VEC_SV+2		; IRQ code vector
 NMI_vec	= IRQ_vec+$0A	; NMI code vector
 
@@ -80,122 +76,22 @@ LAB_stlp:
 ;LAB_dowarm:
 ;	JMP	LAB_WARM		; do EhBASIC warm start
 
-;@cold_start: 
-;	.asciiz "cold start..."
-    
-; Implementation of LOAD using a CFFA1 flash interface if present.
 LOAD:
-; Prompt user for filename to load
-        LDX     #<FilenameString
-        LDY     #>FilenameString
-        JSR     PrintString
-
-; Get filename
-        JSR     GetLine
-
-; If user hit <Esc>, cancel the load
-        BCS     Return1
-
-; If filename was empty, call CFFA1 menu
-        LDA     IN                     ; string length
-        BNE     LoadFile               ; Was is zero length?
-        RTS                            ; and return
-
-LoadFile:
-
-;	jsr krn_open
-;	beq @l5
-;	jsr krn_hexout
-;	jmp Return1
-;@l5:
-
-;	jsr krn_hexout
-        
-        RTS
- 
-Return1:
-        RTS
-; Implementation of SAVE using a CFFA1 flash interface if present.
 SAVE:
         RTS                            ; and return
 
 
-; Print a string
-; Pass address of string in X (low) and Y (high).
-; String must be terminated in a null.
-; Cannot be longer than 256 characters.
-; Registers changed: A, Y
-;
-PrintString:
-        STX Itempl
-        STY Itempl+1
-        LDY #0
-@l1:	LDA (Itempl),Y
-        BEQ done
-        JSR chrout
-        INY
-        BNE @l1       ; if doesn't branch, string is too long
-done:   RTS
-
-; String input routine.
-; Enter characters from the keyboard terminated in <Return> or <ESC>.
-; Characters are echoed.
-; Can be up to 127 characters.
-; Returns:
-;   Length stored at IN (doesn't include zero byte).
-;   Characters stored starting at IN+1 ($0201-$027F, same as Woz Monitor)
-;   String is terminated in a 0 byte.
-;   Carry set if user hit <Esc>, clear if used <Enter> or max string length reached.
-; Registers changed: A, X
-GetLine:
-        LDX  #0                 ; Initialize index into buffer
-loop:
-        JSR  V_INPT		        ; Get character from keyboard
-        BEQ  loop
-        CMP  #CR                ; <Enter> key pressed?
-        BEQ  EnterPressed       ; If so, handle it
-        CMP  #ESC               ; <Esc> key pressed?
-        BEQ  EscapePressed      ; If so, handle it
-        JSR  V_OUTP             ; Echo the key pressed
-        STA  IN+1,X             ; Store character in buffer (skip first length byte)
-        INX                     ; Advance index into buffer
-        CPX  #$7E               ; Buffer full?
-        BEQ  EnterPressed       ; If so, return as if <Enter> was pressed
-        BNE  loop               ; Always taken
-EnterPressed:
-        CLC                     ; Clear carry to indicate <Enter> pressed and fall through
-EscapePressed:
-        LDA  #0
-        STA  IN+1,X             ; Store 0 at end of buffer
-        STX  IN                 ; Store length of string
-        RTS                     ; Return
-
-chrout = krn_chrout
-;chrout:
-	;php
-	;pha
-	;phx
-	;phy
-	 ;jsr krn_chrout
-	;ply
-	;plx
-	;pla
-	;plp
-	;rts
-    
 getkey:
 	jsr krn_getkey
 	cmp #$00 
 	beq @l1
+	toupper
 	sec
 	rts
 @l1:	
 	lda #$00
 	clc
 	rts
-
-FilenameString:
-	.asciiz "FILENAME? "
 
 ; vector tables
 
