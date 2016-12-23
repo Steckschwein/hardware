717ecd7797bf0032eed8a6c63257a290067b715e
Marko Lauke <marko.lauke@googlemail.com>
2016-12-23 11:48:50 +0100

    +blub

--- steckos/tools/date.c
+++ steckos/tools/date.c
@@ -1,13 +1,11 @@
 #include <stdlib.h>
 #include <conio.h>
 #include <time.h>
-#include "../include/spi.h"
-#include "../include/rtc.h"
 
 int main (void)
 {
     time_t t = _systime();
-    cprintf ("%s\n\r", asctime(localtime(&t)));
+    cprintf ("%s\n", asctime(localtime(&t)));
    
     return EXIT_SUCCESS;
 }
