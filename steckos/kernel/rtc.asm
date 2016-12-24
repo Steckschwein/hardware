4c1093dfd85add76cbe1cac53a6df89574eb3dd6
Thomas Woinke <thomas@steckschwein.de>
2016-12-24 12:00:46 +0100

    cleanup

--- steckos/kernel/rtc.asm
+++ steckos/kernel/rtc.asm
@@ -8,22 +8,19 @@
 spi_device_rtc=$76;#%01110110
 
 spi_select_rtc:
-	lda #spi_device_rtc
-	; and via1portb
-	sta via1portb
-	rts
+		lda #spi_device_rtc
+		sta via1portb
+		rts
 
 init_rtc:
-	; disable RTC interrupts
-	; Select SPI SS for RTC
-	lda #spi_device_rtc
-	; and via1portb
-	sta via1portb
+		; disable RTC interrupts
+		; Select SPI SS for RTC
+		jsr spi_select_rtc
 
-	lda #$8f
-	jsr spi_rw_byte
-	lda #$00
-	jsr spi_rw_byte
+		lda #$8f
+		jsr spi_rw_byte
+		lda #$00
+		jsr spi_rw_byte
 
-	; Deselect SPI SS for RTC
-	jmp	spi_deselect
+		; Deselect SPI SS for RTC
+		jmp spi_deselect
