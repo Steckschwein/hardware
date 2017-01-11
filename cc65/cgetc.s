8c1a3c3536021a8db4a66d1161770021fc643dd7
Marko Lauke <marko.lauke@googlemail.com>
2017-01-11 21:01:14 +0100

    +cleanup

--- cc65/cgetc.s
+++
@@ -1,8 +0,0 @@
-;
-;
-; char cgetc (void);
-;
-        .export         _cgetc
-        .import         cursor
-_cgetc: 
-        jmp     ($0286)
--- cc65/spi.h
+++
@@ -1,7 +0,0 @@
-#ifndef _SPI_H
-#define _SPI_H
-
-extern unsigned char __fastcall__ spi_read(void);
-extern unsigned char __fastcall__ spi_write(unsigned char b);
-// void fastcall spi_select(unsigned char d);
-#endif
