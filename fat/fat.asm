dcb41788a9e3702799776da2bde8533af2f96c46
Thomas Woinke <thomas@steckschwein.de>
2017-02-12 21:16:07 +0100

    file overwrite test program

--- fat/fat.asm
+++ fat/fat.asm
@@ -21,45 +21,21 @@
 	jsr krn_open
 	lda errno
 	bne error
+	stx tmp
 
-	
-        lda fd_area + F32_fd::DirEntryLBA+3 , x
-        sta lba_addr+3
-        lda fd_area + F32_fd::DirEntryLBA+2 , x
-        sta lba_addr+2
-        lda fd_area + F32_fd::DirEntryLBA+1 , x
-        sta lba_addr+1
-        lda fd_area + F32_fd::DirEntryLBA+0 , x
-        sta lba_addr+0
-
-        lda fd_area + F32_fd::DirEntryPos , x
-        jsr krn_hexout
-        jsr krn_calc_dirptr_from_entry_nr
-        lda dirptr+1
-        jsr krn_hexout
-        lda dirptr+0
-        jsr krn_hexout
-
-        SetVector sd_blktarget, sd_read_blkptr
-        jsr krn_sd_read_block
-
-        ldy #$00
-@rep:
-        lda (dirptr),y
-        jsr krn_chrout
-        iny
-        cpy #12
-        bne @rep
-
-foo:	jmp foo
-
-	
 	SetVector $2000, sd_write_blkptr
 	SetVector $2000, sd_read_blkptr
 
 	jsr krn_read
 	jsr krn_hexout
 
+	ldx tmp
+	lda #$00
+	sta fd_area + F32_fd::FileSize + 3,x
+	sta fd_area + F32_fd::FileSize + 2,x
+	sta fd_area + F32_fd::FileSize + 1,x
+	lda #$10
+	sta fd_area + F32_fd::FileSize + 0,x
 
 	ldy #$00
 w:
@@ -70,25 +46,12 @@ w:
 	bra w
 
 done:
-;	ldy #$00
-;rep:
-	;lda $2000,y
-	;jsr krn_hexout
-	;lda #' '
-	;jsr krn_chrout
-	;iny
-	;bne rep
+
 
 	jsr krn_write
-	lda errno
-	jsr krn_hexout
-	
-
-
-	jsr krn_close
-	
-	lda #'A'
+	lda #'X'
 	jsr krn_chrout
+	
 
 loop:	jmp loop
 error:
@@ -98,6 +61,6 @@ error:
 	jsr krn_hexout
 	bra loop
 
-filename: .asciiz "FILE.DAT"
+filename: .asciiz "FILE0000.DAT"
 text: .asciiz "Es hat geklappt!"
 tmp: .res 4 
