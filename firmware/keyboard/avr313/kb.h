4ea5e4d7b5d6ef5f97643881b77f1792f9bb624e
Thomas Woinke <thomas@steckschwein.de>
2017-07-22 10:43:53 +0200

    experimental, rudimentary and untested INT1 handling for ps2-mouse

--- firmware/keyboard/avr313/kb.h
+++ firmware/keyboard/avr313/kb.h
@@ -8,6 +8,7 @@
 #define PORT_KB PORTD
 #define CLOCK   2
 #define DATAPIN 0
+#define MOUSE_DATAPIN 1
 
 void init_kb(void);
 void decode(unsigned char sc);
@@ -17,11 +18,19 @@ void put_kbbuff(unsigned char c);
 void put_scanbuff(unsigned char c);
 int  get_scanchar(void);
 
+int  get_mousechar(void);
+
 #define SCAN_BUFF_SIZE 12
  uint8_t scan_buffer[SCAN_BUFF_SIZE];
  uint8_t *scan_inptr;
  uint8_t *scan_outptr;
  uint8_t scan_buffcnt;
+
+#define MOUSE_BUFF_SIZE 12
+ uint8_t mouse_buffer[SCAN_BUFF_SIZE];
+ uint8_t *mouse_inptr;
+ uint8_t *mouse_outptr;
+ uint8_t mouse_buffcnt;
 
 #define KB_BUFF_SIZE 8
  uint8_t kb_buffer[KB_BUFF_SIZE];
