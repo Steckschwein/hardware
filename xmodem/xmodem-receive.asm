d246e7a34dfe5dc9787b95788c08e369ef2383cd
Thomas Woinke <thomas@steckschwein.de>
2017-08-16 17:23:42 +0200

    use acme, piggyback loader, address fixes

--- xmodem/xmodem-receive.asm
+++ xmodem/xmodem-receive.asm
@@ -1,7 +1,36 @@
-.include "../steckos/kernel/kernel.inc"
-.include "../steckos/kernel/uart.inc"
-.include "../steckos/kernel/kernel_jumptable.inc"
-.include "../steckos/asminc/common.inc"
+!src "../steckos/kernel/kernel_jumptable.inc"
+!src "../steckos/kernel/uart.inc"
+*=$1000
+dest = $d000
+
+loop:
+
+a:	lda payload
+b:	sta dest
+
+	inc a+1
+	inc b+1
+	bne loop
+
+	lda a+2
+	cmp #>payload_end
+	bne skip
+
+	lda a+1
+	cmp #<payload_end
+	bne skip
+	bra end
+skip:
+	inc a+2
+	inc b+2
+	bne loop
+
+end:
+	jmp dest
+	
+!align 255,0
+payload:
+!pseudopc $d000 {
 
 ; XMODEM/CRC Receiver for the 65C02
 ;
@@ -57,6 +86,8 @@
 ; zero page variables (adjust these to suit your needs)
 ;
 ;
+addr		=	$36
+
 crc		=	$38		; CRC lo byte  (two byte variable)
 crch		=	$39		; CRC hi byte  
 
@@ -114,10 +145,13 @@ ESC		=	$1b		; ESC to exit
 
 ;		*= 	$7B00		; Start of program (adjust to your needs)
 ;
-		jsr krn_keyin
 		jsr XModem
 
-		jmp $3000
+		lda addr+1
+		jsr krn_hexout
+		lda addr
+		jsr krn_hexout
+		jmp (addr)
 
 
 XModem:		
@@ -130,18 +164,18 @@ StartCrc:	lda	#'C'		; "C" start with CRC mode
 		jsr	Put_Chr		; send it
 		lda	#$FF	
 		sta	retry2		; set loop counter for ~3 sec delay
-		lda	#$00
-               	sta	crc
-		sta	crch		; init CRC value	
+		;lda	#$00
+               	stz	crc
+		stz	crch		; init CRC value	
 		jsr	GetByte		; wait for input
                	bcs	GotByte		; byte received, process it
 		bcc	StartCrc	; resend "C"
 
 StartBlk:	lda	#$FF		; 
 		sta	retry2		; set loop counter for ~3 sec delay
-		lda	#$00		;
-		sta	crc		;
-		sta	crch		; init CRC value	
+		;lda	#$00		;
+		stz	crc		;
+		stz	crch		; init CRC value	
 		jsr	GetByte		; get first byte of block
 		bcc	StartBlk	; timed out, keep waiting...
 GotByte:	cmp	#ESC		; quitting?
@@ -203,9 +237,13 @@ GoodCrc:	ldx	#$02		;
 		beq	CopyBlk		; no, copy all 128 bytes
 		lda	Rbuff,x		; get target address from 1st 2 bytes of blk 1
 		sta	ptr		; save lo address
+		sta	addr
+		jsr krn_hexout
 		inx			;
 		lda	Rbuff,x		; get hi address
 		sta	ptr+1		; save it
+		sta	addr+1
+		jsr krn_hexout
 		inx			; point to first byte of data
 		dec	bflag		; set the flag so we won't get another address		
 CopyBlk:	ldy	#$00		; set offset to zero
@@ -232,8 +270,8 @@ Done:		lda	#ACK		; last block, send ACK and exit.
 ; subroutines
 ;
 ;					;
-GetByte:	lda	#$00		; wait for chr input and cycle timing loop
-		sta	retry		; set low value of timing loop
+GetByte:	;lda	#$00		; wait for chr input and cycle timing loop
+		stz	retry		; set low value of timing loop
 StartCrcLp:	jsr	Get_Chr		; get chr from serial port, don't wait 
 		bcs	GetByte1	; got one, so exit
 		dec	retry		; no character received, so dec counter
@@ -250,38 +288,47 @@ Flush1:		jsr	GetByte		; read the port
 		bcs	Flush		; if chr recvd, wait for another
 		rts			; else done
 ;
-PrintMsg:	ldx	#$00		; PRINT starting message
-PrtMsg1:	lda   	Msg,x		
-		beq	PrtMsg2			
-		jsr	Put_Chr
-		inx
-		bne	PrtMsg1
-PrtMsg2:	rts
-Msg:		.byte	"Begin XMODEM/CRC transfer.  Press <Esc> to abort..."
-		.BYTE  	CR, LF
-               	.byte   0
+;PrintMsg:	ldx	#$00		; PRINT starting message
+;PrtMsg1:	lda   	Msg,x		
+		;beq	PrtMsg2			
+		;jsr	Put_Chr
+		;inx
+		;bne	PrtMsg1
+;PrtMsg2:	rts
+PrintMsg:
+		jsr krn_primm
+Msg:		!text	"Begin XMODEM/CRC transfer."
+		!BYTE  	CR, LF
+		!text	"Press <Esc> to abort..."
+		!BYTE  	CR, LF
+               	!byte   0
+		rts
 ;
-Print_Err:	ldx	#$00		; PRINT Error message
-PrtErr1:	lda   	ErrMsg,x
-		beq	PrtErr2
-		jsr	Put_Chr
-		inx
-		bne	PrtErr1
-PrtErr2:	rts
-ErrMsg:		.byte 	"Upload Error!"
-		.BYTE  	CR, LF
-                .byte   0
+Print_Err:	;ldx	#$00		; PRINT Error message
+;PrtErr1:	lda   	ErrMsg,x
+		;beq	PrtErr2
+		;jsr	Put_Chr
+		;inx
+		;bne	PrtErr1
+;PrtErr2:	rts
+		jsr krn_primm
+ErrMsg:		!text 	"Upload Error!"
+		!BYTE  	CR, LF
+                !byte   0
+		rts
 ;
-Print_Good:	ldx	#$00		; PRINT Good Transfer message
-Prtgood1:	lda   	GoodMsg,x
-		beq	Prtgood2
-		jsr	Put_Chr
-		inx
-		bne	Prtgood1
-Prtgood2:	rts
-GoodMsg:	.byte 	"Upload Successful!"
-		.BYTE  	CR, LF
-                .byte   0
+Print_Good:	;ldx	#$00		; PRINT Good Transfer message
+;Prtgood1:	lda   	GoodMsg,x
+		;beq	Prtgood2
+		;jsr	Put_Chr
+		;inx
+		;bne	Prtgood1
+;Prtgood2:	rts
+		jsr krn_primm
+GoodMsg:	!text 	"Upload Successful!"
+		!BYTE  	CR, LF
+                !byte   0
+		rts
 ;
 ;
 ;======================================================================
@@ -304,11 +351,11 @@ GoodMsg:	.byte 	"Upload Successful!"
 Get_Chr:		
                 lda #$01        ; Maske fuer DataReady Bit
                 bit uart1lsr
-                beq @nobyte
+                beq +
                 lda uart1rxtx
 		sec
 		rts
-@nobyte:
++
 		clc
                 rts
 
@@ -373,46 +420,48 @@ UpdCrc:		eor 	crc+1 		; Quick CRC computation with lookup tables
 ;
 ; low byte CRC lookup table (should be page aligned)
 ;		*= $7D00
-.align 256
+!align 255,0
 crclo:
- .byte $00,$21,$42,$63,$84,$A5,$C6,$E7,$08,$29,$4A,$6B,$8C,$AD,$CE,$EF
- .byte $31,$10,$73,$52,$B5,$94,$F7,$D6,$39,$18,$7B,$5A,$BD,$9C,$FF,$DE
- .byte $62,$43,$20,$01,$E6,$C7,$A4,$85,$6A,$4B,$28,$09,$EE,$CF,$AC,$8D
- .byte $53,$72,$11,$30,$D7,$F6,$95,$B4,$5B,$7A,$19,$38,$DF,$FE,$9D,$BC
- .byte $C4,$E5,$86,$A7,$40,$61,$02,$23,$CC,$ED,$8E,$AF,$48,$69,$0A,$2B
- .byte $F5,$D4,$B7,$96,$71,$50,$33,$12,$FD,$DC,$BF,$9E,$79,$58,$3B,$1A
- .byte $A6,$87,$E4,$C5,$22,$03,$60,$41,$AE,$8F,$EC,$CD,$2A,$0B,$68,$49
- .byte $97,$B6,$D5,$F4,$13,$32,$51,$70,$9F,$BE,$DD,$FC,$1B,$3A,$59,$78
- .byte $88,$A9,$CA,$EB,$0C,$2D,$4E,$6F,$80,$A1,$C2,$E3,$04,$25,$46,$67
- .byte $B9,$98,$FB,$DA,$3D,$1C,$7F,$5E,$B1,$90,$F3,$D2,$35,$14,$77,$56
- .byte $EA,$CB,$A8,$89,$6E,$4F,$2C,$0D,$E2,$C3,$A0,$81,$66,$47,$24,$05
- .byte $DB,$FA,$99,$B8,$5F,$7E,$1D,$3C,$D3,$F2,$91,$B0,$57,$76,$15,$34
- .byte $4C,$6D,$0E,$2F,$C8,$E9,$8A,$AB,$44,$65,$06,$27,$C0,$E1,$82,$A3
- .byte $7D,$5C,$3F,$1E,$F9,$D8,$BB,$9A,$75,$54,$37,$16,$F1,$D0,$B3,$92
- .byte $2E,$0F,$6C,$4D,$AA,$8B,$E8,$C9,$26,$07,$64,$45,$A2,$83,$E0,$C1
- .byte $1F,$3E,$5D,$7C,$9B,$BA,$D9,$F8,$17,$36,$55,$74,$93,$B2,$D1,$F0 
+ !byte $00,$21,$42,$63,$84,$A5,$C6,$E7,$08,$29,$4A,$6B,$8C,$AD,$CE,$EF
+ !byte $31,$10,$73,$52,$B5,$94,$F7,$D6,$39,$18,$7B,$5A,$BD,$9C,$FF,$DE
+ !byte $62,$43,$20,$01,$E6,$C7,$A4,$85,$6A,$4B,$28,$09,$EE,$CF,$AC,$8D
+ !byte $53,$72,$11,$30,$D7,$F6,$95,$B4,$5B,$7A,$19,$38,$DF,$FE,$9D,$BC
+ !byte $C4,$E5,$86,$A7,$40,$61,$02,$23,$CC,$ED,$8E,$AF,$48,$69,$0A,$2B
+ !byte $F5,$D4,$B7,$96,$71,$50,$33,$12,$FD,$DC,$BF,$9E,$79,$58,$3B,$1A
+ !byte $A6,$87,$E4,$C5,$22,$03,$60,$41,$AE,$8F,$EC,$CD,$2A,$0B,$68,$49
+ !byte $97,$B6,$D5,$F4,$13,$32,$51,$70,$9F,$BE,$DD,$FC,$1B,$3A,$59,$78
+ !byte $88,$A9,$CA,$EB,$0C,$2D,$4E,$6F,$80,$A1,$C2,$E3,$04,$25,$46,$67
+ !byte $B9,$98,$FB,$DA,$3D,$1C,$7F,$5E,$B1,$90,$F3,$D2,$35,$14,$77,$56
+ !byte $EA,$CB,$A8,$89,$6E,$4F,$2C,$0D,$E2,$C3,$A0,$81,$66,$47,$24,$05
+ !byte $DB,$FA,$99,$B8,$5F,$7E,$1D,$3C,$D3,$F2,$91,$B0,$57,$76,$15,$34
+ !byte $4C,$6D,$0E,$2F,$C8,$E9,$8A,$AB,$44,$65,$06,$27,$C0,$E1,$82,$A3
+ !byte $7D,$5C,$3F,$1E,$F9,$D8,$BB,$9A,$75,$54,$37,$16,$F1,$D0,$B3,$92
+ !byte $2E,$0F,$6C,$4D,$AA,$8B,$E8,$C9,$26,$07,$64,$45,$A2,$83,$E0,$C1
+ !byte $1F,$3E,$5D,$7C,$9B,$BA,$D9,$F8,$17,$36,$55,$74,$93,$B2,$D1,$F0 
 
 ; hi byte CRC lookup table (should be page aligned)
 ;		*= $7E00
-.align 256
+!align 255,0
 crchi:
- .byte $00,$10,$20,$30,$40,$50,$60,$70,$81,$91,$A1,$B1,$C1,$D1,$E1,$F1
- .byte $12,$02,$32,$22,$52,$42,$72,$62,$93,$83,$B3,$A3,$D3,$C3,$F3,$E3
- .byte $24,$34,$04,$14,$64,$74,$44,$54,$A5,$B5,$85,$95,$E5,$F5,$C5,$D5
- .byte $36,$26,$16,$06,$76,$66,$56,$46,$B7,$A7,$97,$87,$F7,$E7,$D7,$C7
- .byte $48,$58,$68,$78,$08,$18,$28,$38,$C9,$D9,$E9,$F9,$89,$99,$A9,$B9
- .byte $5A,$4A,$7A,$6A,$1A,$0A,$3A,$2A,$DB,$CB,$FB,$EB,$9B,$8B,$BB,$AB
- .byte $6C,$7C,$4C,$5C,$2C,$3C,$0C,$1C,$ED,$FD,$CD,$DD,$AD,$BD,$8D,$9D
- .byte $7E,$6E,$5E,$4E,$3E,$2E,$1E,$0E,$FF,$EF,$DF,$CF,$BF,$AF,$9F,$8F
- .byte $91,$81,$B1,$A1,$D1,$C1,$F1,$E1,$10,$00,$30,$20,$50,$40,$70,$60
- .byte $83,$93,$A3,$B3,$C3,$D3,$E3,$F3,$02,$12,$22,$32,$42,$52,$62,$72
- .byte $B5,$A5,$95,$85,$F5,$E5,$D5,$C5,$34,$24,$14,$04,$74,$64,$54,$44
- .byte $A7,$B7,$87,$97,$E7,$F7,$C7,$D7,$26,$36,$06,$16,$66,$76,$46,$56
- .byte $D9,$C9,$F9,$E9,$99,$89,$B9,$A9,$58,$48,$78,$68,$18,$08,$38,$28
- .byte $CB,$DB,$EB,$FB,$8B,$9B,$AB,$BB,$4A,$5A,$6A,$7A,$0A,$1A,$2A,$3A
- .byte $FD,$ED,$DD,$CD,$BD,$AD,$9D,$8D,$7C,$6C,$5C,$4C,$3C,$2C,$1C,$0C
- .byte $EF,$FF,$CF,$DF,$AF,$BF,$8F,$9F,$6E,$7E,$4E,$5E,$2E,$3E,$0E,$1E 
+ !byte $00,$10,$20,$30,$40,$50,$60,$70,$81,$91,$A1,$B1,$C1,$D1,$E1,$F1
+ !byte $12,$02,$32,$22,$52,$42,$72,$62,$93,$83,$B3,$A3,$D3,$C3,$F3,$E3
+ !byte $24,$34,$04,$14,$64,$74,$44,$54,$A5,$B5,$85,$95,$E5,$F5,$C5,$D5
+ !byte $36,$26,$16,$06,$76,$66,$56,$46,$B7,$A7,$97,$87,$F7,$E7,$D7,$C7
+ !byte $48,$58,$68,$78,$08,$18,$28,$38,$C9,$D9,$E9,$F9,$89,$99,$A9,$B9
+ !byte $5A,$4A,$7A,$6A,$1A,$0A,$3A,$2A,$DB,$CB,$FB,$EB,$9B,$8B,$BB,$AB
+ !byte $6C,$7C,$4C,$5C,$2C,$3C,$0C,$1C,$ED,$FD,$CD,$DD,$AD,$BD,$8D,$9D
+ !byte $7E,$6E,$5E,$4E,$3E,$2E,$1E,$0E,$FF,$EF,$DF,$CF,$BF,$AF,$9F,$8F
+ !byte $91,$81,$B1,$A1,$D1,$C1,$F1,$E1,$10,$00,$30,$20,$50,$40,$70,$60
+ !byte $83,$93,$A3,$B3,$C3,$D3,$E3,$F3,$02,$12,$22,$32,$42,$52,$62,$72
+ !byte $B5,$A5,$95,$85,$F5,$E5,$D5,$C5,$34,$24,$14,$04,$74,$64,$54,$44
+ !byte $A7,$B7,$87,$97,$E7,$F7,$C7,$D7,$26,$36,$06,$16,$66,$76,$46,$56
+ !byte $D9,$C9,$F9,$E9,$99,$89,$B9,$A9,$58,$48,$78,$68,$18,$08,$38,$28
+ !byte $CB,$DB,$EB,$FB,$8B,$9B,$AB,$BB,$4A,$5A,$6A,$7A,$0A,$1A,$2A,$3A
+ !byte $FD,$ED,$DD,$CD,$BD,$AD,$9D,$8D,$7C,$6C,$5C,$4C,$3C,$2C,$1C,$0C
+ !byte $EF,$FF,$CF,$DF,$AF,$BF,$8F,$9F,$6E,$7E,$4E,$5E,$2E,$3E,$0E,$1E 
 ;
 ;
 ; End of File
 ;
+}
+payload_end:
