bda065624cb0416ccb8f9e8a5442469ed4ced120
Thomas Woinke <thomas@steckschwein.de>
2017-08-18 22:27:29 +0200

    faster copyloop

--- steckos/kernel/loader.asm
+++ steckos/kernel/loader.asm
@@ -1,4 +1,3 @@
-.setcpu "65C02"
 .include "common.inc"
 .include "kernel.inc"
 
@@ -7,32 +6,66 @@
 ; 	system attribute has to be set on file system
 
 ; .pages = (.payload_end - .payload) / 256 + 1
-
+src_ptr	= $e0
+dst_ptr = $e2
 		; copy kernel code to $f000
+	
+		lda #>payload
+		sta src_ptr+1
+		stz src_ptr
+
+		lda #>kernel_start
+		sta dst_ptr+1
+		stz dst_ptr
+
+
+;		SetVector payload, src_ptr
+;		SetVector kernel_start, dst_ptr
+
+
+		ldy #$00
 loop:
-
-@a:		lda payload
-@b:		sta kernel_start
-
-		inc @a+1
-		inc @b+1
+		lda (src_ptr),y
+		sta (dst_ptr),y
+		iny
 		bne loop
-
-		lda @a+2
+		lda src_ptr+1
 		cmp #>payload_end
 		bne @skip
-		
-		lda @a+1
-		cmp #<payload_end
+
+		cpy #<payload_end
 		bne @skip
-		bra end
+
 @skip:
-		inc @a+2
-		inc @b+2
+		inc src_ptr+1
+		inc dst_ptr+1
 		bne loop
-
 end:
-
+		
+; loop:
+; 
+; @a:		lda payload
+; @b:		sta kernel_start
+; 
+; 		inc @a+1
+; 		inc @b+1
+; 		bne loop
+; 
+; 		lda @a+2
+; 		cmp #>payload_end
+; 		bne @skip
+; 		
+; 		lda @a+1
+; 		cmp #<payload_end
+; 		bne @skip
+; 		bra end
+; @skip:
+; 		inc @a+2
+; 		inc @b+2
+; 		bne loop
+; 
+; end:
+; 
 		lda #$01
 		sta memctl
 
@@ -45,7 +78,3 @@ end:
 payload:
 .incbin "kernel.bin"
 payload_end:
-
-;.segment "KERNEL"
-;segment "VECTORS"
-;segment "JUMPTABLE"
