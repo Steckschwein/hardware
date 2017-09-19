ed060b66aeb25679c5f41952fca45e6fdc35d044
Marko Lauke <marko.lauke@googlemail.com>
2017-09-19 19:57:30 +0200

    +impl. update fat block

--- steckos/kernel/fat32.asm
+++ steckos/kernel/fat32.asm
@@ -266,37 +266,37 @@ fat_mkdir:
 		cmp	#ENOENT			;error must be no such file or directory, otherwise we wont create a new one
 		bne @l_exit
 
-		jsr __fat_find_free_cluster
+		jsr __fat_find_free_cluster				; find free cluster
 		bne @l_exit
 		
-		jsr __fat_mark_free_cluster
-		
-		debug32 "f_lba", lba_addr			; lba_addr is already setup by __fat_find_free_cluster
-		SetVector	block_fat, write_blkptr
-		lda #0
-;		jsr sd_write_block
+		jsr @__fat_mark_free_cluster			; mark cluster in block with EOC - TODO cluster chain support
+		jsr @__fat_write_block_fat				; write the updated fat block for both FATs to the device
 		bne @l_exit
-		clc
-		lda lba_addr+0
-		;adc 
-		
 		
 		; new dir entry
-		jsr __rtc_systime_update
+		jsr __rtc_systime_update				; update systime struct
 		;debug32 "rtc0", __rtc_systime_t
 		;debug32 "rtc1", __rtc_systime_t+4
 		
 		lda __rtc_systime_t+time_t::tm_hour
 		lda __rtc_systime_t+time_t::tm_min
 		lda __rtc_systime_t+time_t::tm_sec
-		sta @dir_entry_template+F32DirEntry::CrtTime
-		sta @dir_entry_template+F32DirEntry::WrtTime
+		lda #<(14<<11 | 1<<5 | 33)
+		sta @dir_entry_template+F32DirEntry::CrtTime+0
+		sta @dir_entry_template+F32DirEntry::WrtTime+0
+		lda #>(14<<11 | 1<<5 | 33)
+		sta @dir_entry_template+F32DirEntry::CrtTime+1
+		sta @dir_entry_template+F32DirEntry::WrtTime+1
 
 		lda __rtc_systime_t+time_t::tm_year
 		lda __rtc_systime_t+time_t::tm_mon
 		lda __rtc_systime_t+time_t::tm_mday
-		sta @dir_entry_template+F32DirEntry::CrtTime
-		sta @dir_entry_template+F32DirEntry::WrtTime
+		lda #<((2017-1980) <<9 | 9 <<5 | 19)
+		sta @dir_entry_template+F32DirEntry::CrtDate+0
+		sta @dir_entry_template+F32DirEntry::WrtDate+0
+		lda #>((2017-1980) <<9 | 9 <<5 | 19)
+		sta @dir_entry_template+F32DirEntry::CrtDate+1
+		sta @dir_entry_template+F32DirEntry::WrtDate+1
 		
 		lda #0
 		bra @l_exit
@@ -325,11 +325,27 @@ fat_mkdir:
 		.word 0					;clnr low
 		.dword 0				;file size
 		
+@__fat_write_block_fat:
+		debug32 "f_lba", lba_addr			; lba_addr is already setup by __fat_find_free_cluster
+		SetVector	block_fat, write_blkptr
+		lda #0
+;		jsr sd_write_block
+		bne @l_exit				
+		clc									; calc fat2 lba_addr = lba_addr + VolumeID::FATSz32
+		.repeat 4, i
+			lda lba_addr + i
+			adc volumeID + VolumeID::FATSz32 + i
+			sta lba_addr + i
+		.endrepeat
+		debug32 "f2_lba", lba_addr
+;		jsr sd_write_block
+		rts
+			
 		; in:
 		;	Y - offset in block
 		; 	read_blkptr - points to block_fat either 1st or 2nd page
-__fat_mark_free_cluster:
-		debugdump "block", block_fat+$190
+@__fat_mark_free_cluster:
+;		debugdump "block", block_fat+$190
 		lda #$ff
 		sta (read_blkptr), y
 		iny
@@ -339,7 +355,7 @@ __fat_mark_free_cluster:
 		iny
 		lda #$0f
 		sta (read_blkptr), y
-		debugdump "block", block_fat+$190		
+;		debugdump "block", block_fat+$190		
 		rts
 		
 __fat_find_free_cluster:
@@ -637,15 +653,14 @@ calc_lba_addr:
 		cmp #$ff
 		beq file_not_open
 
-		; lba_addr = cluster_begin_lba_m2 + (cluster_number * sectors_per_cluster);
+		; lba_addr = cluster_begin_lba_m2 + (cluster_number * VolumeID::SecPerClus);
 		.repeat 4,i
 			lda fd_area + F32_fd::StartCluster + i,x
 			sta lba_addr + i
 		.endrepeat
 
-		;sectors_per_cluster -> is a power of 2 value, therefore cluster << n, where n ist the number of bit set in sectors_per_cluster
+		;SecPerClus is a power of 2 value, therefore cluster << n, where n ist the number of bit set in VolumeID::SecPerClus
 		lda volumeID+VolumeID::SecPerClus
-		;lda sectors_per_cluster
 @lm:	lsr
 		beq @lme    ; 1 sector/cluster therefore skip multiply
 		tax
@@ -829,10 +844,6 @@ fat_mount:
 		sta errno
 		jmp end_mount
 @l6:
-		; Sectors per Cluster. Valid: 1,2,4,8,16,32,64,128
-;		lda volumeID+VolumeID::SecPerClus
-;		sta sectors_per_cluster
-
 		; cluster_begin_lba = Partition_LBA_Begin + Number_of_Reserved_Sectors + (Number_of_FATs * Sectors_Per_FAT) -  (2 * sec/cluster);
 		; fat_lba_begin		= Partition_LBA_Begin + Number_of_Reserved_Sectors
 		; fat2_lba_begin	= Partition_LBA_Begin + Number_of_Reserved_Sectors + Sectors_Per_FAT
@@ -874,28 +885,28 @@ fat_mount:
 		bne @l7
 	
 		; calc begin of 2nd fat (end of 1st fat)
+		; TODO FIXME - we assume 16bit are sufficient for now since fat is placed at the beginning of the device
 		clc
-		lda sd_blktarget + VolumeID::FATSz32+0 ; sectors/blocks per fat
+		lda volumeID + VolumeID::FATSz32+0 ; sectors/blocks per fat
 		adc fat_lba_begin	+0
 		sta fat2_lba_begin	+0
-		lda sd_blktarget + VolumeID::FATSz32+1
+		lda volumeID + VolumeID::FATSz32+1
 		adc fat_lba_begin	+1
 		sta fat2_lba_begin	+1
-		; TODO FIXME - we assume 16bit are sufficient for now since fat is placed at the beginning of the device
+		
 
-		; cluster_begin_lba_m2 -> cluster_begin_lba - (BPB_RootClus*sec/cluster)
+		; cluster_begin_lba_m2 -> cluster_begin_lba - (VolumeID::RootClus*VolumeID::SecPerClus)
 		debug8 "sec/cl", volumeID+VolumeID::SecPerClus
 		debug32 "cl_lba", cluster_begin_lba
 		debug32 "lba", lba_addr
-		debug16 "r_sec", sd_blktarget + VolumeID::RsvdSecCnt
-		debug32 "f_sec", sd_blktarget + VolumeID::FATSz32
+		debug16 "r_sec", volumeID + VolumeID::RsvdSecCnt
+		debug32 "f_sec", volumeID + VolumeID::FATSz32
 		debug16 "f_lba", fat_lba_begin
 		debug16 "f2_lba", fat2_lba_begin
 
-		;TODO FIXME we assume 2 here insteasd of using the value in BPB_RootClus
-		; cluster_begin_lba_m2 -> cluster_begin_lba - (2*sec/cluster) -> sec/cluster << 1
+		;TODO FIXME we assume 2 here insteasd of using the value in VolumeID::RootClus
+		; cluster_begin_lba_m2 -> cluster_begin_lba - (2*sec/cluster) => cluster_begin_lba - (sec/cluster << 1)
 		lda volumeID+VolumeID::SecPerClus ; max sec/cluster can be 128, with 2 (BPB_RootClus) * 128 wie may subtract max 256
-		;lda sectors_per_cluster
 		asl
 		sta lba_addr        ;   used as tmp
 		stz lba_addr +1     ;   safe carry
@@ -927,7 +938,6 @@ end_mount:
 		Copy root_dir_first_clus, fd_area + FD_INDEX_CURRENT_DIR + F32_fd::StartCluster, 3
 		ldx #FD_INDEX_CURRENT_DIR
 		rts
-
 
 		;
 		; out:
