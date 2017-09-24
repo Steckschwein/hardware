dcdaa3b1929ab0203e902b2802cb06c7dc688dc3
Thomas Woinke <thomas@steckschwein.de>
2017-09-24 20:36:05 +0200

    moved rx, mkdist.sh put tools in different dirs

--- steckos/mkdist.sh
+++ steckos/mkdist.sh
@@ -1,20 +1,32 @@
 #!/bin/bash
 
 TARGET=$1
-TOOLS="clear.prg ls.prg ll.prg rename.prg date.prg nvram.prg setdate.prg keycode.prg view.prg"
+TOOLS_BIN="clear.prg ls.prg ll.prg rename.prg date.prg keycode.prg view.prg rmdir.prg mkdir.prg"
+TOOLS_SBIN="nvram.prg setdate.prg"
+TOOLS_USRBIN="view.prg"
 
 rm -r dist/*
 mkdir -p dist/BIN
+mkdir -p dist/SBIN
+mkdir -p dist/USR/BIN
 cp kernel/loader.bin dist/LOADER.BIN
 cp shell/shell.prg dist/SHELL.PRG
 
-for n in $TOOLS ; do
+for n in $TOOLS_BIN ; do
 	un=`echo $n | awk '{print toupper($0)}'`
 	cp tools/$n dist/BIN/$un
 done
 
-#^for i in {0..9} ; do 
-#	echo foobar > dist/FILE$i.DAT
-#done
+for n in $TOOLS_SBIN ; do
+	un=`echo $n | awk '{print toupper($0)}'`
+	cp tools/$n dist/SBIN/$un
+done
+
+for n in $TOOLS_USRBIN ; do
+	un=`echo $n | awk '{print toupper($0)}'`
+	cp tools/$n dist/USR/BIN/$un
+done
+
+cp tools/xmodem/rx.prg dist/BIN/RX.PRG
 
 cp -a dist/* $TARGET &&  umount $TARGET
