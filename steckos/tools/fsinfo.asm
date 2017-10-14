de128c28d7eea52b7a55b42bd17c15a9d7e0b1c7
Thomas Woinke <thomas@steckschwein.de>
2017-10-14 18:03:49 +0200

    ref #26 fsinfo sector stub

--- steckos/tools/fsinfo.asm
+++ steckos/tools/fsinfo.asm
@@ -1,8 +1,13 @@
 .include "kernel.inc"
 .include "kernel_jumptable.inc"
+.include "common.inc"
+
 .include "../kernel/fat32.inc"
+.include "../kernel/sdcard.inc"
+
 .include "appstart.inc"
 
+data = $2000
 volid = $0308
 appstart $1000
 
@@ -56,6 +61,35 @@ appstart $1000
 
         crlf
 
+        jsr krn_primm
+        .byte "FSInfo sector     : $",$00
+
+        lda volid + VolumeID::FSInfoSec+1
+        sta sd_cmd_param+1
+        jsr krn_hexout
+        lda volid + VolumeID::FSInfoSec
+        sta sd_cmd_param+0
+        jsr krn_hexout
+
+;       stz sd_cmd_param+0
+;        stz sd_cmd_param+1
+        stz sd_cmd_param+2
+        stz sd_cmd_param+3
+        stz sd_cmd_chksum
+
+        crlf
+;        SetVector data, read_blkptr
+;        jsr krn_sd_read_block
+
+
+;        lda data
+;        jsr krn_hexout
+;        lda data+1
+;        jsr krn_hexout
+;        lda data+2
+;        jsr krn_hexout
+;        lda data+3
+;        jsr krn_hexout
 
 
 
