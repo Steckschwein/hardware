447cbeb4bfcc5b993f3499dbc2f0697fbae07673
Thomas Woinke <thomas@steckschwein.de>
2017-10-15 12:25:01 +0200

    close #26 - read bootsector for partition info - read fsinfo sector and show free clusters

--- steckos/tools/fsinfo.asm
+++ steckos/tools/fsinfo.asm
@@ -7,12 +7,63 @@
 
 .include "appstart.inc"
 
-data = $2000
+;data = $2000
 volid = $0308
+part0 = data + BootSector::Partitions + PartTable::Partition_0
+
 appstart $1000
 
+        stz lba_addr+0
+        stz lba_addr+1
+        stz lba_addr+2
+        stz lba_addr+3
+
+        SetVector data, read_blkptr
+        jsr krn_sd_read_block
+
+        m_memcpy part0 + PartitionEntry::LBABegin, lba_tmp, 4
+
+
         jsr krn_primm
-        .byte "Bytes / sector    : $",$00
+        .byte "Partition info: ",$0a,"Bootable      :",$00
+        lda part0 + PartitionEntry::Bootflag
+        jsr krn_hexout
+
+        jsr krn_primm
+        .byte $0a,"Type Code     :$", $00
+        lda part0 + PartitionEntry::TypeCode
+        jsr krn_hexout
+        crlf
+
+        jsr krn_primm
+        .byte "Begin LBA     :$", $00
+        lda part0 + PartitionEntry::LBABegin+3
+        jsr krn_hexout
+        lda part0 + PartitionEntry::LBABegin+2
+        jsr krn_hexout
+        lda part0 + PartitionEntry::LBABegin+1
+        jsr krn_hexout
+        lda part0 + PartitionEntry::LBABegin+0
+        jsr krn_hexout
+
+        jsr krn_primm
+        .byte $0a, "Size (sectors):$", $00
+        lda part0 + PartitionEntry::NumSectors+3
+        jsr krn_hexout
+        lda part0 + PartitionEntry::NumSectors+2
+        jsr krn_hexout
+        lda part0 + PartitionEntry::NumSectors+1
+        jsr krn_hexout
+        lda part0 + PartitionEntry::NumSectors+0
+        jsr krn_hexout
+
+
+        jsr krn_primm
+        .byte $0a,$0a,"Filesystem Info: ",$0a,$00
+
+
+        jsr krn_primm
+        .byte "Bytes / sector    :$",$00
 
         lda volid + VolumeID::BytsPerSec+1
         jsr krn_hexout
@@ -22,7 +73,7 @@ appstart $1000
         crlf
 
         jsr krn_primm
-        .byte "Sectors / cluster : $",$00
+        .byte "Sectors / cluster :$",$00
 
         lda volid + VolumeID::SecPerClus
         jsr krn_hexout
@@ -30,7 +81,7 @@ appstart $1000
         crlf
 
         jsr krn_primm
-        .byte "Number of FATs    : $",$00
+        .byte "Number of FATs    :$",$00
 
         lda volid + VolumeID::NumFATs
         jsr krn_hexout
@@ -38,7 +89,7 @@ appstart $1000
         crlf
 
         jsr krn_primm
-        .byte "Reserved sectors  : $",$00
+        .byte "Reserved sectors  :$",$00
 
         lda volid + VolumeID::RsvdSecCnt+1
         jsr krn_hexout
@@ -48,7 +99,7 @@ appstart $1000
         crlf
 
         jsr krn_primm
-        .byte "Sectors / FAT     : $",$00
+        .byte "Sectors / FAT     :$",$00
 
         lda volid + VolumeID::FATSz32+3
         jsr krn_hexout
@@ -62,35 +113,61 @@ appstart $1000
         crlf
 
         jsr krn_primm
-        .byte "FSInfo sector     : $",$00
+        .byte "FSInfo sector LBA :$",$00
+
+        lda volid + VolumeID::FSInfoSec
+        clc
+        adc lba_tmp+0
+        sta lba_addr+0
 
         lda volid + VolumeID::FSInfoSec+1
-        sta sd_cmd_param+1
+        adc lba_tmp+1
+        sta lba_addr+1
+
+        lda #$00
+        adc lba_tmp+2
+        sta lba_addr+2
+        lda #$00
+        adc lba_tmp+3
+        sta lba_addr+3
+
+        lda lba_addr+3
         jsr krn_hexout
-        lda volid + VolumeID::FSInfoSec
-        sta sd_cmd_param+0
+        lda lba_addr+2
+        jsr krn_hexout
+        lda lba_addr+1
+        jsr krn_hexout
+        lda lba_addr+0
         jsr krn_hexout
 
-;       stz sd_cmd_param+0
-;        stz sd_cmd_param+1
-        stz sd_cmd_param+2
-        stz sd_cmd_param+3
-        stz sd_cmd_chksum
+        SetVector data, read_blkptr
+        jsr krn_sd_read_block
+
+        jsr krn_primm
+        .byte $0a,"Free clusters     :$",$00
+        lda data+488+3
+        jsr krn_hexout
+        lda data+488+2
+        jsr krn_hexout
+        lda data+488+1
+        jsr krn_hexout
+        lda data+488+0
+        jsr krn_hexout
+
+        jsr krn_primm
+        .byte $0a,"Next free cluster :$",$00
+        lda data+492+3
+        jsr krn_hexout
+        lda data+492+2
+        jsr krn_hexout
+        lda data+492+1
+        jsr krn_hexout
+        lda data+492+0
+        jsr krn_hexout
 
         crlf
-;        SetVector data, read_blkptr
-;        jsr krn_sd_read_block
-
-
-;        lda data
-;        jsr krn_hexout
-;        lda data+1
-;        jsr krn_hexout
-;        lda data+2
-;        jsr krn_hexout
-;        lda data+3
-;        jsr krn_hexout
-
-
 
         jmp (retvec)
+lba_tmp: .res 4
+.align 255,0
+data:
