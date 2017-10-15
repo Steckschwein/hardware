4c67b367ec5b906ffcb5644c7e9702db1f6345e3
Thomas Woinke <thomas@steckschwein.de>
2017-10-15 19:22:59 +0200

    ref #26 - decimal

--- steckos/tools/fsinfo.asm
+++ steckos/tools/fsinfo.asm
@@ -23,7 +23,6 @@ appstart $1000
 
         m_memcpy part0 + PartitionEntry::LBABegin, lba_tmp, 4
 
-
         jsr krn_primm
         .byte "Partition info: ",$0a,"Bootable      :",$00
         lda part0 + PartitionEntry::Bootflag
@@ -38,31 +37,23 @@ appstart $1000
         crlf
 
         jsr krn_primm
-        .byte "Begin LBA     :$", $00
-        lda part0 + PartitionEntry::LBABegin+3
-        jsr krn_hexout
-        lda part0 + PartitionEntry::LBABegin+2
-        jsr krn_hexout
-        lda part0 + PartitionEntry::LBABegin+1
-        jsr krn_hexout
-        lda part0 + PartitionEntry::LBABegin+0
-        jsr krn_hexout
+        .byte "Begin LBA     :", $00
+        m_memcpy part0 + PartitionEntry::LBABegin, tmp0, 4
+        jsr BINBCD32
 
+        ldx #$05
+        jsr display_bcd
         jsr krn_primm
-        .byte $0a, "Size (sectors):$", $00
-        lda part0 + PartitionEntry::NumSectors+3
-        jsr krn_hexout
-        lda part0 + PartitionEntry::NumSectors+2
-        jsr krn_hexout
-        lda part0 + PartitionEntry::NumSectors+1
-        jsr krn_hexout
-        lda part0 + PartitionEntry::NumSectors+0
-        jsr krn_hexout
+        .byte $0a, "Size (sectors):", $00
 
+        m_memcpy part0 + PartitionEntry::NumSectors, tmp0, 4
+        jsr BINBCD32
+
+        ldx #$05
+        jsr display_bcd
 
         jsr krn_primm
         .byte $0a,$0a,"Filesystem Info: ",$0a,$00
-
 
         jsr krn_primm
         .byte "Bytes / sector    :",$00
@@ -70,6 +61,7 @@ appstart $1000
         ldx volid + VolumeID::BytsPerSec+1
         lda volid + VolumeID::BytsPerSec
         jsr BINBCD16
+        ldx #$02
         jsr display_bcd
 
 
@@ -79,6 +71,7 @@ appstart $1000
         lda volid + VolumeID::SecPerClus
         ldx #$00
         jsr BINBCD16
+        ldx #$02
         jsr display_bcd
 
         jsr krn_primm
@@ -110,25 +103,20 @@ appstart $1000
         ldx volid + VolumeID::RsvdSecCnt+1
         lda volid + VolumeID::RsvdSecCnt
         jsr BINBCD16
+        ldx #$02
         jsr display_bcd
         crlf
 
         jsr krn_primm
-        .byte "Sectors / FAT     :$",$00
+        .byte "Sectors / FAT     :",$00
 
-        lda volid + VolumeID::FATSz32+3
-        jsr krn_hexout
-        lda volid + VolumeID::FATSz32+2
-        jsr krn_hexout
-        lda volid + VolumeID::FATSz32+1
-        jsr krn_hexout
-        lda volid + VolumeID::FATSz32+0
-        jsr krn_hexout
-
-        crlf
+        m_memcpy volid + VolumeID::FATSz32, tmp0, 4
+        jsr BINBCD32
+        ldx #$05
+        jsr display_bcd
 
         jsr krn_primm
-        .byte "FSInfo sector LBA :$",$00
+        .byte $0a,"FSInfo sector LBA :",$00
 
         lda volid + VolumeID::FSInfoSec
         clc
@@ -139,6 +127,7 @@ appstart $1000
         adc lba_tmp+1
         sta lba_addr+1
 
+
         lda #$00
         adc lba_tmp+2
         sta lba_addr+2
@@ -146,40 +135,30 @@ appstart $1000
         adc lba_tmp+3
         sta lba_addr+3
 
-        lda lba_addr+3
-        jsr krn_hexout
-        lda lba_addr+2
-        jsr krn_hexout
-        lda lba_addr+1
-        jsr krn_hexout
-        lda lba_addr+0
-        jsr krn_hexout
+        m_memcpy lba_addr, tmp0, 4
+
+        jsr BINBCD32
+        ldx #$05
+        jsr display_bcd
+
 
         SetVector data, read_blkptr
         jsr krn_sd_read_block
 
         jsr krn_primm
-        .byte $0a,"Free clusters     :$",$00
-        lda data+488+3
-        jsr krn_hexout
-        lda data+488+2
-        jsr krn_hexout
-        lda data+488+1
-        jsr krn_hexout
-        lda data+488+0
-        jsr krn_hexout
+        .byte $0a,"Free clusters     :",$00
+        m_memcpy data+488, tmp0, 4
+        jsr BINBCD32
+        ldx #$05
+        jsr display_bcd
 
         jsr krn_primm
-        .byte $0a,"Next free cluster :$",$00
-        lda data+492+3
-        jsr krn_hexout
-        lda data+492+2
-        jsr krn_hexout
-        lda data+492+1
-        jsr krn_hexout
-        lda data+492+0
-        jsr krn_hexout
-
+        .byte $0a,"Next free cluster :",$00
+        m_memcpy data+492, tmp0, 4
+        jsr BINBCD32
+        ldx #$05
+        jsr display_bcd
+        
         crlf
 
         jmp (retvec)
@@ -194,7 +173,7 @@ BINBCD16:
         STA BCD+2
         LDX #16         ; The number of source bits
 
-CNVBIT:
+@CNVBIT:
         ASL tmp0+0       ; Shift out one bit
         ROL tmp0+1
         LDA BCD+0       ; And add into result
@@ -207,28 +186,70 @@ CNVBIT:
         ADC BCD+2
         STA BCD+2
         DEX             ; And repeat for next bit
-        BNE CNVBIT
+        BNE @CNVBIT
+        CLD             ; Back to binary
+
+        rts
+
+BINBCD32:
+        lda tmp0
+        SED             ; Switch to decimal mode
+        LDA #0          ; Ensure the result is clear
+        STA BCD+0
+        STA BCD+1
+        STA BCD+2
+        STA BCD+3
+        STA BCD+4
+        STA BCD+5
+
+
+        LDX #32         ; The number of source bits
+
+@CNVBIT:
+        ASL tmp0+0       ; Shift out one bit
+        ROL tmp0+1
+        ROL tmp0+2
+        ROL tmp0+3
+
+        LDA BCD+0       ; And add into result
+        ADC BCD+0
+        STA BCD+0
+        LDA BCD+1       ; propagating any carry
+        ADC BCD+1
+        STA BCD+1
+        LDA BCD+2       ; ... thru whole result
+        ADC BCD+2
+        STA BCD+2
+        LDA BCD+3       ; ... thru whole result
+        ADC BCD+3
+        STA BCD+3
+        LDA BCD+4       ; ... thru whole result
+        ADC BCD+4
+        STA BCD+4;        LDA BCD+5       ; ... thru whole result
+        ADC BCD+5
+        STA BCD+5
+
+        DEX             ; And repeat for next bit
+        BNE @CNVBIT
         CLD             ; Back to binary
 
         rts
 
 display_bcd:
-        lda BCD + 2
-        beq @n1
+        ;ldx #$05
+        @l:
+        lda BCD,x
+;        beq @out
         jsr krn_hexout
-@n1:
-        lda BCD + 1
-        beq @n2
-        jsr krn_hexout
-@n2:
-        lda BCD + 0
-        jsr krn_hexout
+@out:
+        dex
+        bpl @l
         rts
 
 
 
 lba_tmp: .res 4
-BCD: .res 3
+BCD: .res 6
 tmp0: .res 2
 .align 255,0
 data:
