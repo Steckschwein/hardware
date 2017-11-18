a7e62a198d49e9dcf61d4aa6055bf8e9fbda6898
Thomas Woinke <thomas@steckschwein.de>
2017-11-18 21:38:59 +0100

    sqrt8 algorithm

---
+++ sqrt8/sqrt8.asm
@@ -0,0 +1,31 @@
+.include "../steckos/kernel/kernel.inc"
+.include "../steckos/kernel/kernel_jumptable.inc"
+.include "../steckos/asminc/common.inc"
+.include "../steckos/asminc/appstart.inc"
+appstart $1000
+
+			lda #25
+			sta $20
+
+			jsr sqrt8
+
+			lda $20
+			jsr krn_hexout
+
+			jmp (retvec)
+
+
+sqrt8:		ldy #$00
+			lda #$00
+			sta $21
+			lda $20
+again:		cmp $21
+			bcc nomore
+			sbc $21
+			iny
+			inc $21
+			inc $21
+			bra again
+nomore:		sty $20
+			sta $21
+			rts
