9b7b5dc99f444c071a7a7f893f6bc3196f2177eb
Marko Lauke <marko.lauke@googlemail.com>
2018-08-01 22:14:15 +0200

    ref #42 +output

--- asmunit/asmunit_wrapper.sh
+++ asmunit/asmunit_wrapper.sh
@@ -3,9 +3,9 @@
 dir=`dirname $0`
 logfile=$1.log
 $dir/asmunit_runner.sh $1 $2 > $logfile
-
-test_count=`egrep "(PASS|FAIL)" $logfile | wc -l`
-#echo $test_count
+test_ok=`grep "PASS" $logfile | wc -l`
+test_fail=`grep "FAIL" $logfile | wc -l`
+echo "Tests run: "`expr $test_ok + $test_fail`", Failures: ${test_fail}"
 if [ -n "`grep FAIL $logfile`" ] ; then
         cat $logfile
         exit 1
