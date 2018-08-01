95110a047110f80d7d55d8cc1a66b284d0040b4c
Marko Lauke <marko.lauke@googlemail.com>
2018-08-01 22:28:11 +0200

    ref #42 +more tests

---
+++ steckos/kernel/test/test.textui.asm
@@ -0,0 +1,20 @@
+	.include "assertion.inc" 	; test api
+	.include "zeropage.inc"
+	
+	.import textui_chrout, textui_put
+	.import textui_update_crs_ptr, textui_crsxy
+
+.segment "KERNEL"	; test must be placed into kernel segment, cuz we wanna use the same linker config
+
+		ldx #0
+		ldy #0
+		jsr textui_crsxy
+		
+		ldx #1
+		ldy #3
+		jsr textui_crsxy
+		
+		assert8 1, crs_x
+		assert8 3, crs_y
+		
+	.include "asmunit.asm"
