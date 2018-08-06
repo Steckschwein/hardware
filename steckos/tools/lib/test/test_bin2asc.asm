f71bc6d264f1be11e08b67396e44c386c733c015
Thomas Woinke <thomas@steckschwein.de>
2018-08-06 21:58:05 +0200

    more tests

--- steckos/tools/lib/test/test_bin2asc.asm
+++ steckos/tools/lib/test/test_bin2asc.asm
@@ -1,4 +1,6 @@
 	.import b2ad2				; uut
+	.import b2ad				; uut
+    .import dpb2ad
 	.include "assertion.inc" 	; test api
 
 	; required to run with steckos only
@@ -8,15 +10,35 @@
 ;	.byte   1
 
 .code
+    ldx #0
+    ldy #0
+
+	lda	#42
+	jsr	b2ad
+
+	assertOut "42"	; assert outpuz
+    assertX 0
+    assertY 0
+
 	lda	#042
 	jsr	b2ad2
 
 	assertOut "042"	; assert outpuz
+    assertX 0
+    assertY 0
 
 	lda	#255
 	jsr	b2ad2
 
 	assertOut "255"	; assert outpuz
+
+	lda	#255
+	ldx	#255
+	jsr	dpb2ad
+
+	assertOut "65535"	; assert outpuz
+
+
 
 	; lda	#$e7
 	; jsr	hexout
