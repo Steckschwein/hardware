4b5175ff643b504e892f007aa458d77151f9c62f
Thomas Woinke <thomas@steckschwein.de>
2018-10-12 15:14:59 +0200

    test fsutil routines

---
+++ steckos/tools/lib/test/test_fsutil.asm
@@ -0,0 +1,64 @@
+.include "asmunit.inc" 	; unit test api
+.include "kernel.inc"
+
+.include "fat32.inc"
+
+.import dir_show_entry, pagecnt, entries_per_page, dir_attrib_mask
+.import dword2asc
+.import char_out
+.import print_filesize,print_fat_date,print_fat_time, print_filename
+
+.code
+    lda #<direntry
+    sta dirptr
+    lda #>direntry
+    sta dirptr+1
+
+    test_name "fat_entry_filesize"
+
+
+    jsr print_filesize
+    assertOut "246543"
+
+
+    test_name "fat_entry_wrtdate"
+
+    ldy #F32DirEntry::WrtDate
+    jsr print_fat_date
+
+    assertOut "00.0"
+
+    test_name "fat_entry_wrttime"
+
+    ldy #F32DirEntry::WrtTime +1
+    jsr print_fat_time
+
+    assertOut "00:0"
+
+
+    test_name "fat_entry_filename"
+
+    jsr print_filename
+    assertOut "FOOBAR  BAZ"
+
+
+	brk
+
+cnt: 	.byte $04
+dirs:	.byte $00
+files:	.byte $00
+direntry:
+    .byte "FOOBAR  BAZ" ; filename+ext
+    .byte 0             ; attribute
+    .byte 0             ; Reserved
+    .byte 0             ; CrtTimeMillis
+    .word $0000         ; CrtTimeMillis
+    .word $0000         ; CrtDate
+    .word $0000         ; LstModDate
+    .word $0000         ; FstClusHI
+    .word $0000         ; WrtTime
+    .word $0000         ; WrtDate
+    .word $0000         ; FstClusLO
+    .dword 246543       ; $03c30f
+
+.segment "ASMUNIT"
