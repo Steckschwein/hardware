0befc8c6889305c817ba18fa17a4afb3cfea475a
Marko Lauke <marko.lauke@googlemail.com>
2018-10-13 15:45:11 +0200

    +doc

--- steckos/tools/lib/test/test_hexout.asm
+++ steckos/tools/lib/test/test_hexout.asm
@@ -1,8 +1,11 @@
 .include "asmunit.inc" 	; unit test api
 
-.import hexout					; uut
+.import hexout		; uut
 	
-.code	
+.code
+
+	test_name "hexout"
+
 	lda	#$7e
 	jsr	hexout
 	
@@ -12,15 +15,16 @@
 	lda	#$e7
 	jsr	hexout
 	
-	assertOut "E7"	; assert outpuz
-	assertA $e7		; assert A is not destroyed
+	assertOut "E7"	;
+	assertA $e7		;
 	
 	lda	#$9f
-	jsr	hexout
+	
+	jsr	hexout	
 	
 	assertOut "9F"
 	assertA $9f	
-
+	
 	brk
 	
 .segment "ASMUNIT"
