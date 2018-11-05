a76963d3038f5860ef6c0b81a16967997f936c1f
Marko Lauke <marko.lauke@googlemail.com>
2018-11-05 09:22:59 +0100

    ref #42 +clean code, copyright

--- asmunit/asmunit.monitor.py
+++ asmunit/asmunit.monitor.py
@@ -1,3 +1,25 @@
+# MIT License
+#
+# Copyright (c) 2018 Thomas Woinke, Marko Lauke, www.steckschwein.de
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE. 
+
 #!/usr/bin/env python -u 
 
 import py65.monitor
@@ -5,7 +27,6 @@ import py65.monitor
 class AsmunitMonitor(py65.monitor.Monitor):
 
     def _install_mpu_observers(self, getc_addr, putc_addr):
-        #print("_install_mpu_observers()")
         instrument_addr = 0x0202
 		  
         super(AsmunitMonitor, self)._install_mpu_observers(getc_addr, putc_addr)
