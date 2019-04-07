39e20dde7333f8494f793b418aa509c94e41d27a
Thomas Woinke <thomas@steckschwein.de>
2019-04-07 16:05:56 +0200

    k,hdc

--- firmware/keyboard/main.c
+++ firmware/keyboard/main.c
@@ -61,12 +61,8 @@ int __attribute__((OS_main)) main(void)
 
 	init_kb();
 	sei();
+    // send(0xee);
 
-    /*
-    send(0xff);
-    while(1) {
-    };
-    */
 
 	spiInitSlave();
 #ifdef SERIAL_DEBUG
