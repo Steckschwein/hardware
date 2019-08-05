4337ccb05edf372e87d879baf7e8202316a3d450
Thomas Woinke <thomas@steckschwein.de>
2019-08-05 18:19:03 +0200

    pin names, irq and nmi lines

--- glue/decoder.vhd
+++ glue/decoder.vhd
@@ -7,25 +7,28 @@ Entity decoder is
 	Port (
 		-- system clock
 		CLKIN		 : in std_logic;
-		PHI2OUT	 : out std_logic;
+		CPU_phi2	 : out std_logic;
 		
 		-- address bus
-		A			 : in std_logic_vector (15 downto 0);	-- Address bus 
-		AO			 : out std_logic_vector(18 downto 13);   -- extended address bus AO13-AO18
+		CPU_a			 : in std_logic_vector (15 downto 0);	-- Address bus 
+		EXT_a			 : out std_logic_vector(18 downto 13);   -- extended address bus EXT_a13-EXT_a18
 		
 		-- data bus
-		D			 : inout std_logic_vector (7 downto 0); -- data bus lower 4 bits
+		CPU_d			 : inout std_logic_vector (7 downto 0); -- data bus lower 4 bits
 		
 		-- control signals
 		RESET     : in std_logic;  	-- reset line
-		RW        : in std_logic;  	-- RW pin of 6502
-		RDY		 : out std_logic; 	-- RDY signal for generating wait states
+		CPU_rw    : in std_logic;  	-- RW pin of 6502
+		CPU_rdy	 : out std_logic; 	-- RDY signal for generating wait states
 		RD			 : out std_logic; 	-- read access
 		WR			 : out std_logic; 	-- write access
 		--CPU
 		CPU_vp	 : in std_logic;
 		CPU_be	 : out std_logic;
 		CPU_sync	 : in std_logic;
+		CPU_irq	 : in std_logic;
+		CPU_nmi	 : in std_logic;
+		
 		
 		
 		-- chip select for memory
@@ -53,7 +56,7 @@ Architecture decoder_arch of decoder is
 	signal rdyclk: STD_LOGIC;
 	signal romoff: std_logic;
 	signal rom_bank: std_logic_vector(1 downto 0);
-	signal AO_sig : std_logic_vector(18 downto 13);   -- extended address bus AO13-AO18
+	signal EXT_a_sig : std_logic_vector(18 downto 13);   -- extended address bus EXT_a13-EXT_a18
 
 	signal cs_rom_sig: std_logic;
 	signal cs_ram_sig: std_logic;
@@ -78,20 +81,20 @@ Architecture decoder_arch of decoder is
 begin
 	-- inputs
 	clk		<= CLKIN;
-	d_in 		<= D;	
+	d_in 		<= CPU_d;	
 
 	-- bidirectional
 	-- make data bus output tristate when not a qualified read
-	D 			<= d_out when (is_read='1') else (others => 'Z');
+	CPU_d 	<= d_out when (is_read='1') else (others => 'Z');
 	
 	-- outputs
-	PHI2OUT 	<= clk;
-	RD 		<= RW nand clk;
-	WR 		<= not RW nand clk;
-	RD_OPL	<= not RW;
-	WR_OPL	<= RW;
-	RDY		<= rdy_sig;
-	AO			<= AO_sig;
+	CPU_phi2	<= clk;
+	RD 		<= CPU_rw nand clk;
+	WR 		<= not CPU_rw nand clk;
+	RD_OPL	<= not CPU_rw;
+	WR_OPL	<= CPU_rw;
+	CPU_rdy	<= rdy_sig;
+	EXT_a		<= EXT_a_sig;
 		
 	cs_uart 		<= cs_uart_sig;
 	cs_via  		<= cs_via_sig;
@@ -107,12 +110,12 @@ begin
 	
 	-- helpers
 	-- qualified read?
-	is_read 		<= reg_select and clk and rw;
+	is_read 		<= reg_select and clk and CPU_rw;
 	
 	-- internal register selected ($0230 - $023f)
-	reg_select  	<= '1' when (A(15 downto 4) = "000000100011") else '0';						-- $0230
+	reg_select  	<= '1' when (CPU_a(15 downto 4) = "000000100011") else '0';						-- $0230
 	
-	reg_addr 		<= A(0);
+	reg_addr 		<= CPU_a(0);
 	
 	-- cpu register section
 	-- cpu read
@@ -139,24 +142,24 @@ begin
 	end process;
 
 	-- cpu write 
-	cpu_write: process(reset, reg_select, reg_addr, clk, RW, D_in)
+	cpu_write: process(reset, reg_select, reg_addr, clk, CPU_rw, D_in)
 	begin
 		if (reset = '0') then
 			CPU_be		<= '1';
 			romoff 		<= '0';
 			rom_bank 	<= "00";
-			AO_sig(18)	<= '0'; -- A18
-			AO_sig(17) 	<= '0'; -- A17
-			AO_sig(16) 	<= '0'; -- A16
-		elsif (falling_edge(clk) and reg_select='1' and RW='0') then
+			EXT_a_sig(18)	<= '0'; -- A18
+			EXT_a_sig(17) 	<= '0'; -- A17
+			EXT_a_sig(16) 	<= '0'; -- A16
+		elsif (falling_edge(clk) and reg_select='1' and CPU_rw='0') then
 			case reg_addr is
 				when '0' =>         
 					romoff 		<= D_in(0);
 					rom_bank(0) <= D_in(1);
 					rom_bank(1) <= D_in(2);				 
-					AO_sig(16) 	<= D_in(3); -- A16
-					AO_sig(17) 	<= D_in(4); -- A17
-					AO_sig(18)	<= D_in(5); -- A18
+					EXT_a_sig(16) 	<= D_in(3); -- A16
+					EXT_a_sig(17) 	<= D_in(4); -- A17
+					EXT_a_sig(18)	<= D_in(5); -- A18
 				when others =>
 			end case;
 		end if;
@@ -187,26 +190,26 @@ begin
 	
 	-- io area decoding
 	
-	CS_UART_sig    <= '0' when (A(15 downto 4) = "000000100000") else '1'; 						-- $0200		
-	CS_VIA_sig     <= '0' when (A(15 downto 4) = "000000100001") else '1'; 						-- $0210
+	CS_UART_sig    <= '0' when (CPU_a(15 downto 4) = "000000100000") else '1'; 						-- $0200		
+	CS_VIA_sig     <= '0' when (CPU_a(15 downto 4) = "000000100001") else '1'; 						-- $0210
 	
-	CSR_VDP_sig		<= '0' when (A(15 downto 4) = "000000100010") and (RW = '1') else '1'; 	-- $0220	
-	CSW_VDP_sig		<= '0' when (A(15 downto 4) = "000000100010") and (RW = '0') else '1'; 	-- $0220	
-	CS_OPL_sig		<= '0' when (A(15 downto 4) = "000000100100") else '1';  					-- $0240
-	cs_io01_sig 	<= '0' when (A(15 downto 4) = "000000100101") else '1'; 						-- $0250
-	cs_io02_sig 	<= '0' when (A(15 downto 4) = "000000100110") else '1';						-- $0260
+	CSR_VDP_sig		<= '0' when (CPU_a(15 downto 4) = "000000100010") and (CPU_rw = '1') else '1'; 	-- $0220	
+	CSW_VDP_sig		<= '0' when (CPU_a(15 downto 4) = "000000100010") and (CPU_rw = '0') else '1'; 	-- $0220	
+	CS_OPL_sig		<= '0' when (CPU_a(15 downto 4) = "000000100100") else '1';  					-- $0240
+	cs_io01_sig 	<= '0' when (CPU_a(15 downto 4) = "000000100101") else '1'; 						-- $0250
+	cs_io02_sig 	<= '0' when (CPU_a(15 downto 4) = "000000100110") else '1';						-- $0260
 
 
-	cs_rom_sig	  	<= '0' when (ROMOFF = '0') and (RW = '1') and (A(15 downto 13) = "111") else '1';
+	cs_rom_sig	  	<= '0' when (ROMOFF = '0') and (CPU_rw = '1') and (CPU_a(15 downto 13) = "111") else '1';
 	cs_ram_sig		<= '0' when (
-							not(A(15 downto 7) = "000000100") -- io area $0200 - $027f
+							not(CPU_a(15 downto 7) = "000000100") -- io area $0200 - $027f
 							and cs_rom_sig='1'
 						) else '1';
 						
-	ao_sig(13) 		<= rom_bank(0) when cs_rom_sig = '0' else A(13);
-	ao_sig(14) 		<= rom_bank(1) when cs_rom_sig = '0' else A(14);
+	EXT_a_sig(13) 		<= rom_bank(0) when cs_rom_sig = '0' else CPU_a(13);
+	EXT_a_sig(14) 		<= rom_bank(1) when cs_rom_sig = '0' else CPU_a(14);
 	
-	ao_sig(15) 		<= A(15);
+	EXT_a_sig(15) 		<= CPU_a(15);
 	
 	
 
