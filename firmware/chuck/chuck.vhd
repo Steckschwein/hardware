244b4b7728d1e4b8aa586ecff04aea9491aaf39b
mlauke <marko.lauke@googlemail.com>
2023-07-06 01:08:56 +0200

    +spec.

--- firmware/chuck/chuck.vhd
+++ firmware/chuck/chuck.vhd
@@ -2,6 +2,7 @@ library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 use IEEE.STD_LOGIC_UNSIGNED.ALL;
 USE ieee.numeric_std.ALL;
+USE ieee.math_real.log2;
 
 
 Entity chuck is
@@ -50,8 +51,11 @@ end;
 Architecture chuck_arch of chuck is
 
    -- calculation constant
-   constant SYS_CLOCK:   integer := 8; -- system clock
-   constant CLK_DIVIDER: integer := 2; -- amount of bits for the clock divider
+   constant HW_CLOCK:      integer := 16; -- board input clock (oszi)
+   constant SYS_CLOCK:     integer := 2; -- desired system clock (cpu)
+   
+   constant CLOCK_DIV:        integer := HW_CLOCK/2/SYS_CLOCK; -- clock divider
+   constant CLOCK_DIV_BITS:   integer := integer(log2(real(HW_CLOCK/2)))-1; -- amount of bits for the clock divider
 
    -- define bank table type array of 6 bit vectors
    type t_banktable is array (0 to 3) of std_logic_vector(5 downto 0);
@@ -59,7 +63,7 @@ Architecture chuck_arch of chuck is
 
    signal clk: std_logic;
 
-   signal clk_div: std_logic_vector(CLK_DIVIDER downto 0); -- 6 bit counter
+   signal clk_div: std_logic_vector(CLOCK_DIV_BITS downto 0); -- n bit counter
    signal rdy_en: boolean;
 
    signal d_out: std_logic_vector(7 downto 0);
@@ -164,9 +168,9 @@ begin
       end if;
    end process;
 
-	clk <= clk_div(2) AND '1';
+	clk <= clk_div(integer(log2(real(CLOCK_DIV))));
 
-   sig_acs <= '1' when conv_integer(clk_div) < (SYS_CLOCK-1) else '0';
+   sig_acs <= '1' when conv_integer(clk_div)/CLOCK_DIV < (SYS_CLOCK-1) else '0';
 
    -- wait state generator
    --process(clk, clk_div, rdy_en)
